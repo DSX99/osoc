1573b28ae2cbf85442a026a656fe72e8966284e9
tracer-ysyx <tracer@ysyx.org>
2026-06-10 13:39:38 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  13:39:38 up  3:04,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -115,7 +115,7 @@ static bool make_token(char *e) {
       }
     }
 
-    if (i == NR_REGEX) {
+    if (i == NR_REGEX && position!=strlen(e)) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
