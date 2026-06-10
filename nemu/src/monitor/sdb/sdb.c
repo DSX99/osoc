61a1edf208c36e23d6d6aab02ae70387234fa583
tracer-ysyx <tracer@ysyx.org>
2026-06-10 10:44:17 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  10:44:17 up 8 min,  1 user,  load average: 0.23, 0.09, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -61,12 +61,23 @@ static int cmd_si(char *args) {
   }else{
     long val = strtol(args, &endptr, 0);
     if(*endptr != '\0'){
-      printf("Correct use si N , where N is an integer");
+      printf("Correct use si N , where N is an integer.\n");
       return 0;
     }
     cpu_exec(val);
     return 0;
   }
+}
+
+static int cmd_info(char *args) {
+  if((*args) == 'r'){
+    isa_reg_display();
+  }else if ((*args) == 'w'){
+    
+  }else{
+    printf("Correct use info SUBCMD , where SUBCMD is r for register info or w for watchpoint info.\n");
+  }
+  return 0;
 }
 
 static int cmd_help(char *args);
@@ -80,7 +91,7 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si", " si N Lets the program pause after executing N instructions using single step execution, when N is not given, the default is 1", cmd_si },
-  // { "info", " info SUBCMD info r:Print register status info w:Print watchpoint information", cmd_info },
+  { "info", " info SUBCMD info r:Print register status info w:Print watchpoint information", cmd_info },
   // { "x", " x N EXPR Finds the value of the expression EXPR, uses the result as the starting memory address, and outputs consecutive N 4 bytes in hexadecimal.", cmd_x },
   // { "p", " p EXPR Find the value of the expression EXPR, for EXPR supported operations", cmd_p },
   // { "w", " w EXPR Suspend program execution when the value of expression EXPR changes.", cmd_w },
