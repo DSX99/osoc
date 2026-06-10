603b499630eaef7e2a2aa46a6cc0c41477b46789
tracer-ysyx <tracer@ysyx.org>
2026-06-10 21:59:44 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  21:59:44 up 20 min,  1 user,  load average: 0.06, 0.03, 0.00

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -48,10 +48,10 @@ static char gen_rand_op(){
 
 static void gen_rand_expr(char **point) {
   int i = rand()%3;
-  if(*point - buf > 20) i=0;
+  if(*point - buf > 6000) i=0;
   switch (i) {
     case 0: 
-      *point+=sprintf(*point,"%d",(unsigned)(rand()+1)); 
+      *point+=sprintf(*point,"%u",(unsigned)(rand()+1)); 
         break;
     case 1: 
       *point+=sprintf(*point,"%c",'(');
