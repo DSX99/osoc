6988ccc1915d425ce6970a3529999b0afd371993
tracer-ysyx <tracer@ysyx.org>
2026-06-10 22:27:29 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:27:29 up 47 min,  1 user,  load average: 0.02, 0.02, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -103,6 +103,7 @@ static bool make_token(char *e) {
           return 0;
         }
         strncpy(tokens[nr_token].str,substr_start,substr_len);
+        tokens[nr_token].str[substr_len] = '\0';
         nr_token++;
 
         /* TODO: Now a new token is recognized with rules[i]. Add codes
