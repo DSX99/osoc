5ea1cbee2abdada37b907970b3df30bd9a47eb1a
tracer-ysyx <tracer@ysyx.org>
2026-06-11 12:12:29 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  12:12:29 up  1:52,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -37,6 +37,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   printf("%s\n",s);
   for(int i=0;i<32;i++){
     if(strcmp(s,regs[i])){
+      printf("got reg %d\n",i);
       return gpr(i);
     }
   }
