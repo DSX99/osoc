98fa4b8eef644e0ecdd96e0e31dfcb101dffc7c0
tracer-ysyx <tracer@ysyx.org>
2026-06-11 08:18:21 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:18:21 up  2:19,  1 user,  load average: 0.16, 0.07, 0.01

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
     int val = expr(ptr,&check);
     // printf("%u %u: %u \n", atoi(cmd), val, atoi(cmd)-val);
     if(atoi(cmd)-val != 0){
-      printf("%s %u\n\n",buf, atoi(cmd)-val);
+      printf("%s %u\n\n",ptr, atoi(cmd)-val);
     }
   }
   return 0;
