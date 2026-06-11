fad3109c998bf1344c13586372fba13495c0e341
tracer-ysyx <tracer@ysyx.org>
2026-06-11 18:38:33 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  18:38:33 up  2:23,  1 user,  load average: 0.00, 0.02, 0.00

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -1,26 +1,34 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #include "sdb.h"
 
-#define NR_WP 32
+#define NR_WP 64
 
-typedef struct watchpoint {
+typedef struct token
+{
+  int type;
+  char str[32];
+} Token;
+
+typedef struct watchpoint
+{
   int NO;
   struct watchpoint *next;
-
+  char expr[256];
+  uint32_t value;
   /* TODO: Add more members if necessary */
 
 } WP;
@@ -28,9 +36,11 @@ typedef struct watchpoint {
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
-void init_wp_pool() {
+void init_wp_pool()
+{
   int i;
-  for (i = 0; i < NR_WP; i ++) {
+  for (i = 0; i < NR_WP; i++)
+  {
     wp_pool[i].NO = i;
     wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
   }
@@ -39,5 +49,95 @@ void init_wp_pool() {
   free_ = wp_pool;
 }
 
-/* TODO: Implement the functionality of watchpoint */
+WP *new_wp()
+{
 
+  if (free_->next == NULL)
+  {
+    printf("No more free WP");
+  }
+
+  WP *first_free = free_;
+
+  if (head == NULL)
+  {
+    head = first_free;
+    free_ = first_free->next;
+    return first_free;
+  }
+  else
+  {
+    WP *head_wp = head;
+    while (head_wp->next != NULL)
+    {
+      head_wp = head_wp->next;
+    }
+    head_wp->next = first_free;
+    free_ = first_free->next;
+    return first_free;
+  }
+}
+
+void free_wp(WP *wp)
+{
+  WP *head_wp = head;
+  while (head_wp->next != wp)
+  {
+    head_wp = head_wp->next;
+  }
+  if (wp->next == NULL)
+  {
+    head_wp->next = NULL;
+  }
+  else
+  {
+    head_wp->next = wp->next;
+  }
+
+  WP *first_free = free_->next;
+
+  while ((wp->NO < first_free->NO))
+  {
+    first_free = first_free->next;
+  }
+  free_ = wp;
+  wp->next = first_free;
+  return;
+}
+
+void create_wp(char *s){
+  bool success=1;
+  WP *wp = new_wp();
+  strcpy(wp->expr,s);
+  wp->value = expr(s,&success);
+  if(!success){
+    printf("Error in evaluating starting val for wp");
+    free_wp(wp);
+    return;
+  }
+  printf("Added wp with id:%d, and EXPR:%s",wp->NO, s);
+  return;
+}
+
+bool check_wp(WP *wp){
+  bool success=1;
+  uint32_t val = expr(wp->expr,&success);
+  if(!success){
+    printf("Error in evaluating starting val for wp");
+    free_wp(wp);
+    return 1;
+  }
+  if(val == wp->value){
+    return 1;
+  }
+  return 0;
+}
+
+bool check_watchpoints(){
+  while(head->next!=NULL){
+    
+  }
+  return 0;
+}
+
+/* TODO: Implement the functionality of watchpoint */
