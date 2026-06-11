00bee848b3ae67249da50e2f3a577861925a9741
tracer-ysyx <tracer@ysyx.org>
2026-06-11 07:43:16 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:43:16 up  1:43,  1 user,  load average: 0.21, 0.09, 0.02

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -25,6 +25,7 @@ word_t expr(char *e, bool *success);
 
 int main(int argc, char *argv[]) {
   char s[1024*16];
+  char *ptr = s;
   FILE *fp = fopen("/home/dsx99/osoc/ysyx-workbench/nemu/tools/gen-expr/input_2", "r");
   if (fp == NULL){
     printf("No file");
@@ -35,7 +36,8 @@ int main(int argc, char *argv[]) {
     printf("No file");
     exit(EXIT_FAILURE);
   }
-  char *cmd = strtok(s, " ");
+  char *cmd = strtok(ptr, " ");
+  ptr = ptr + strlen(cmd) + 1;
   if (cmd == NULL) { return 1; }
   bool check=0;
   int val = expr(s,&check);
