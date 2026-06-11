477505765d56bb898270beed2f1c2b0c2cd7131b
tracer-ysyx <tracer@ysyx.org>
2026-06-11 07:45:12 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:45:12 up  1:45,  1 user,  load average: 0.03, 0.06, 0.01

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
   ptr = ptr + strlen(cmd) + 1;
   if (cmd == NULL) { return 1; }
   bool check=0;
-  int val = expr(s,&check);
+  int val = expr(ptr,&check);
   printf("%u %u: %u \n", atoi(cmd), val, atoi(cmd)-val);
   return 0;
   /* Initialize the monitor. */
