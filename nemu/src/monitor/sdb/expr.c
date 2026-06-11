707ed09e535b575c29a208c62d289bb4774b140f
tracer-ysyx <tracer@ysyx.org>
2026-06-11 08:13:11 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  08:13:11 up  2:13,  1 user,  load average: 0.03, 0.05, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -94,8 +94,8 @@ static bool make_token(char *e) {
 
         if(i==0 || i==1) {position += substr_len; break;}
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        //     i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
