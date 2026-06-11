04b349d20e1b2b305c35e445f3892faf924ff751
tracer-ysyx <tracer@ysyx.org>
2026-06-11 07:03:51 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:03:51 up  1:04,  1 user,  load average: 0.49, 0.18, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -133,8 +133,8 @@ int check_parentheses(int p, int q){
     if(tokens[q].type==')'){
       int count=0;
       for(int i=q;i>p;i--){
-        if(tokens[i].type=='(') count++;
-        if(tokens[i].type==')') count--;
+        if(tokens[i].type==')') count++;
+        if(tokens[i].type=='(') count--;
         if(count<0) return 0;
       }
     }else{
