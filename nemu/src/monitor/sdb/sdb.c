984ecb8cf5c9cfe14660c08bfedcd0d95bea3327
tracer-ysyx <tracer@ysyx.org>
2026-06-12 07:32:56 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.6.114.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Mon Dec  1 20:46:23 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  07:32:56 up  1:23,  1 user,  load average: 0.03, 0.06, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -121,7 +121,7 @@ static int cmd_p(char *args) {
   bool ret=1;
   bool *p=&ret;
   char *size_str = strtok(args, " ");
-  if(*size_str == 'h'){
+  if(*size_str == 'h' || *size_str == 'H'){
     args = args + strlen(size_str) + 1;
     printf("0x%08x\n",expr(args,p));
   }else{
