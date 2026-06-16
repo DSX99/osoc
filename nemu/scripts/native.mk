d7743b00b033b791827a516c4a5d179f4c219419
tracer-ysyx <tracer@ysyx.org>
2026-06-16 08:35:21 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  08:35:21 up  2:23,  1 user,  load average: 0.22, 0.11, 0.08

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -36,7 +36,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
 	$(call git_commit, "run NEMU")
-	$(NEMU_EXEC) CONFIG_MEMORY_TRACE=1
+	$(NEMU_EXEC) 
 
 runb: run-env
 	$(call git_commit, "run NEMU")
