501e1a348ba19fbc230b1cf1b7407bc5a24441c1
tracer-ysyx <tracer@ysyx.org>
2026-06-17 17:22:43 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:22:43 up  1:53,  1 user,  load average: 0.17, 0.09, 0.09

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -7,10 +7,21 @@
 uint8_t mem[MEM_SIZE];
 
 
+static const uint32_t img [] = {
+  0x00000297,  // auipc t0,0
+  0x00028823,  // sb  zero,16(t0)
+  0x0102c503,  // lbu a0,16(t0)
+  0x00100073,  // ebreak (used as nemu_trap)
+  0xdeadbeef,  // some data
+};
+
+
+
 extern "C" {
     void loadmemory(char *img_file) {
         if (img_file == NULL) {
             printf("No image is given.");
+            memcpy(mem, img, sizeof(img));
             return; // built-in image size
         }
 
