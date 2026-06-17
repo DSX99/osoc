5c9d7434d1337976670462c70f03adc5e243aba7
tracer-ysyx <tracer@ysyx.org>
2026-06-17 12:04:50 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:04:50 up  6:06,  1 user,  load average: 0.27, 0.16, 0.10

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -19,7 +19,7 @@ void loadmemory(char *img_file) {
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
 
-  printf("The image is %s, size = %ld", img_file, size);
+  printf("The image is %s, size = %ld\n", img_file, size);
 
   fseek(fp, 0, SEEK_SET);
   int ret = fread(mem, size, 1, fp);
--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -72,6 +72,7 @@ int main(int argc, char** argv) {
 
 
   reset(top, 100);
+  printf("Starting simu\n");
   if(batch){
     while (contextp->gotFinish()) {
       contextp->timeInc(1);
