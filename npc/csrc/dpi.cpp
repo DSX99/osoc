e48cca9eada16afa23361201e4ceab72afeeaac8
tracer-ysyx <tracer@ysyx.org>
2026-06-17 12:12:20 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:12:20 up  6:13,  1 user,  load average: 0.12, 0.17, 0.12

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -3,7 +3,8 @@
 #include <assert.h>
 
 #define ROM_OFFSET 0x80000000
-uint8_t mem[1024*1024];
+#define MEM_SIZE 1024*1024
+uint8_t mem[MEM_SIZE];
 
 void loadmemory(char *img_file) {
   if (img_file == NULL) {
@@ -30,9 +31,9 @@ void loadmemory(char *img_file) {
 
 extern "C" {
     void memwrite(uint32_t addr, uint32_t data, uint32_t type){
-        if(addr>ROM_OFFSET){
+        if(addr<ROM_OFFSET || addr>(ROM_OFFSET + MEM_SIZE)){
             printf("Illegal memory write access at addr:0x%08x\n",addr);
-            assert(0);
+            return 0;
         }
         if(type ==0){
             mem[addr-ROM_OFFSET] = data & 0xFF;
@@ -50,9 +51,9 @@ extern "C" {
     }
 
     int memread(uint32_t addr){
-        if(addr>ROM_OFFSET){
+        if(addr<ROM_OFFSET || addr>(ROM_OFFSET + MEM_SIZE)){
             printf("Illegal memory read access at addr:0x%08x\n",addr);
-            assert(0);
+            return 0;
         }
         return ((mem[addr-ROM_OFFSET+3]<<24)|(mem[addr-ROM_OFFSET+2]<<16)|(mem[addr-ROM_OFFSET+1]<<8)|(mem[addr-ROM_OFFSET]));
     }
