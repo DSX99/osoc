1375c6553847805a264fb2e626c5f46aab836322
tracer-ysyx <tracer@ysyx.org>
2026-06-18 07:29:28 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:29:28 up  1:01,  1 user,  load average: 0.18, 0.12, 0.09

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -26,7 +26,9 @@ Vtop* top;
 
 void execute(uint32_t n);
 void init_sdb();
+extern "C"{
 void init_disasm();
+}
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
 
 void reset(Vtop *top,int n){
