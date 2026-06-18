0556a59fcdc3e7b450b9886cb0d92d449973ca9b
tracer-ysyx <tracer@ysyx.org>
2026-06-18 12:06:44 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:06:44 up  5:39,  1 user,  load average: 0.15, 0.07, 0.07

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -64,7 +64,7 @@ static int parse_args(int argc, char *argv[]) {
 }
 
 int main(int argc, char** argv) {
-  printf("\n\033[1m\033[36mNPC\033[0m\n\n");
+  printf("\n\033[1m\033[44;1;31mNPC\033[0m\n\n");
   parse_args(argc, argv);
   loadmemory(img_file);
   init_disasm();
