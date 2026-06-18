d0bef9f256875c91a3dbdf7949cfca32ed4b8cfe
tracer-ysyx <tracer@ysyx.org>
2026-06-18 16:00:00 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  16:00:00 up 24 min,  1 user,  load average: 0.15, 0.17, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -188,7 +188,7 @@ void execute(uint32_t n){
 
     difftest_regcpy(ref_regs, 0);
     for(int i=0;i<32;i++){
-      printf("%u:%u",i,regs[i]);
+      printf("%u:%u\n",i,ref_regs[i]);
       // if(ref_regs[i]-top->top->reg_mod->regs[i]!=0){
       //   printf("Difference with REF %s, should:%u, actually:%u\n", regs[i], ref_regs[i], top->top->reg_mod->regs[i]);
       //   ret=1;
