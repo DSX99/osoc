904c31264a2bc6eab6d528d79a5102eb94ec2eb4
tracer-ysyx <tracer@ysyx.org>
2026-06-18 12:25:42 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:25:42 up  5:58,  1 user,  load average: 0.14, 0.16, 0.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -110,6 +110,15 @@ int main(int argc, char** argv) {
   return ret;
 }
 
+
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+
 void execute(uint32_t n){
   difftest_exec(n);
 
@@ -187,13 +196,6 @@ void execute(uint32_t n){
     }
   }
 }
-
-const char *regs[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
 
 void reg_display() {
   printf("Regs values:\n");
