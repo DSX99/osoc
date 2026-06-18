7ab06f16d08c1354fdd79c0605ad6a2f04a22de2
tracer-ysyx <tracer@ysyx.org>
2026-06-18 09:38:34 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:38:34 up  3:10,  1 user,  load average: 0.16, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -123,15 +123,15 @@ void execute(uint32_t n){
       printf("time:%lu\n", contextp->time());
     }
 
-    inst[0] = (top->top->opcode) & 0xff;
-    inst[1] = (top->top->opcode >> 8) & 0xff;
-    inst[2] = (top->top->opcode >> 16) & 0xff;
-    inst[3] = (top->top->opcode >> 24) & 0xff;
-    printf("%08x: %02x %02x %02x %02x \n", top->top->pc, inst[3], inst[2], inst[1], inst[0]);
-    printf("%08x\n", memread(top->top->pc));
-    disassemble(str, 128, top->top->pc, inst, 4);
-    
-    printf("%s\n", str);
+    if(!batch){
+      inst[0] = (top->top->opcode) & 0xff;
+      inst[1] = (top->top->opcode >> 8) & 0xff;
+      inst[2] = (top->top->opcode >> 16) & 0xff;
+      inst[3] = (top->top->opcode >> 24) & 0xff;
+      printf("%08x: %02x %02x %02x %02x ", top->top->pc, inst[3], inst[2], inst[1], inst[0]);
+      disassemble(str, 128, top->top->pc, inst, 4);
+      printf("%s\n", str);
+    }
 
     contextp->timeInc(1);
     top->clk=!top->clk;
