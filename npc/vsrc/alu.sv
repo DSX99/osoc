1d5a6142e9f49901a701fe3d3008c1c15c752bec
tracer-ysyx <tracer@ysyx.org>
2026-06-18 09:35:00 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:35:00 up  3:07,  1 user,  load average: 0.06, 0.16, 0.10

--- npc/vsrc/alu.sv
+++ npc/vsrc/alu.sv
@@ -18,8 +18,8 @@ module alu (
 
     logic [31:0] val1, val2;
 
-    assign val1 = alu_op[7] ? pc : data_rs1;
-    assign val2 = alu_op[6] ? imm : data_rs2;
+    assign val1 = alu_op[6] ? pc : data_rs1;
+    assign val2 = alu_op[7] ? imm : data_rs2;
 
     always_comb begin
         data_out=0;
@@ -29,7 +29,7 @@ module alu (
             case(alu_op[2:0])
                 0: begin
                     if(alu_op[3]) data_out = val1 - val2;
-                    else data_out = val1 + val2;
+                    else data_out = val1 + val2 + 1;
                 end 
                 1: data_out = val1<<val2[4:0];
                 2: data_out = {31'b0,$signed(val1)<$signed(val2)};
