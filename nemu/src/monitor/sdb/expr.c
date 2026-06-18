e32fb7c8705eb9e658d38732614ded57ee2f8581
tracer-ysyx <tracer@ysyx.org>
2026-06-18 08:25:24 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  08:25:24 up  1:57,  1 user,  load average: 0.01, 0.06, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -128,9 +128,6 @@ static bool make_token(char *e)
           return 0;
         }
         tokens[nr_token].type = rules[i].token_type;
-        if(rules[i].token_type=='v'){
-          substr_len= substr_len-1;
-        }
         strncpy(tokens[nr_token].str, substr_start, substr_len);
         tokens[nr_token].str[substr_len + 1] = '\0';
         nr_token++;
