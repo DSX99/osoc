b8c968ef5636c74bf0ad007f323d0e889dada4bb
tracer-ysyx <tracer@ysyx.org>
2026-06-20 08:29:51 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  08:29:51 up  1:24,  1 user,  load average: 0.23, 0.13, 0.05

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -289,6 +289,8 @@ void sdb_mainloop() {
       if (strcmp(cmd, cmd_table[i].name) == 0) {
         if ((hand = cmd_table[i].handler(args)) < 0) { 
           if(hand == -1 && nemu_state.state!=NEMU_END){
+            nemu_state.state = NEMU_ABORT;
+          }else if(hand == -1){
             nemu_state.state = NEMU_QUIT;
           }
           return; 
