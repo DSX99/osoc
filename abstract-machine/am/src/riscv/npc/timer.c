fc5ecc2cb0183aa8aa9942667b78a7b41049e8eb
tracer-ysyx <tracer@ysyx.org>
2026-06-22 09:58:23 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:58:23 up  3:49,  1 user,  load average: 0.83, 0.84, 0.66

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -4,14 +4,14 @@
 uint64_t __start_time;
 
 void __am_timer_init() {
-  volatile uint32_t high = *((uint32_t *)(RTC_ADDR+4));
-  volatile uint32_t low = *((uint32_t *)RTC_ADDR);
+  uint32_t high = *((volatile uint32_t *)(RTC_ADDR+4));
+  uint32_t low = *((volatile uint32_t *)RTC_ADDR);
   __start_time = ((uint64_t)high<<32) + (uint64_t)low;
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  volatile uint32_t high = *((uint32_t *)(RTC_ADDR+4));
-  volatile uint32_t low = *((uint32_t *)RTC_ADDR);
+  uint32_t high = *((volatile uint32_t *)(RTC_ADDR+4));
+  uint32_t low = *((volatile uint32_t *)RTC_ADDR);
   uptime->us = ((uint64_t)high<<32) + (uint64_t)low - __start_time;
 }
 
