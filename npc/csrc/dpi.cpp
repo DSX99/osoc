77fd72d2d1fc1855e2e1d98f6d6332ff1b866d04
tracer-ysyx <tracer@ysyx.org>
2026-06-22 06:55:49 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  06:55:49 up 46 min,  1 user,  load average: 0.02, 0.06, 0.05

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -2,10 +2,19 @@
 #include <stdint.h>
 #include <assert.h>
 #include <cstring>
+#include <time.h>
 
 #define ROM_OFFSET 0x80000000
 #define MEM_SIZE 1024*1024
 uint8_t mem[MEM_SIZE];
+time_t curr_time;
+
+#define DEVICE_BASE 0x00000000 // <- all zeros
+
+#define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
+#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+#define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+#define VGACTL_ADDR     (DEVICE_BASE + 0x0000100)
 
 
 static const uint32_t img [] = {
@@ -47,30 +56,43 @@ extern "C" {
     }
 
     void memwrite(uint32_t addr, uint32_t data, uint32_t type){
-        if(addr<ROM_OFFSET || addr>(ROM_OFFSET + MEM_SIZE)){
-            printf("Illegal memory write access at addr:0x%08x\n",addr);
-            return;
-        }
-        if(type ==0){
-            mem[addr-ROM_OFFSET] = data & 0xFF;
-        }else if (type ==1){
-            mem[addr-ROM_OFFSET] = data & 0xFF;
-            mem[addr-ROM_OFFSET+1] = (data>>8) & 0xFF;
-        }else if (type ==2){
-            mem[addr-ROM_OFFSET] = data & 0xFF;
-            mem[addr-ROM_OFFSET+1] = (data>>8) & 0xFF;
-            mem[addr-ROM_OFFSET+2] = (data>>16) & 0xFF;
-            mem[addr-ROM_OFFSET+3] = (data>>24) & 0xFF;
+        if(addr>ROM_OFFSET || addr<(ROM_OFFSET + MEM_SIZE)){
+            if(type ==0){
+                mem[addr-ROM_OFFSET] = data & 0xFF;
+            }else if (type ==1){
+                mem[addr-ROM_OFFSET] = data & 0xFF;
+                mem[addr-ROM_OFFSET+1] = (data>>8) & 0xFF;
+            }else if (type ==2){
+                mem[addr-ROM_OFFSET] = data & 0xFF;
+                mem[addr-ROM_OFFSET+1] = (data>>8) & 0xFF;
+                mem[addr-ROM_OFFSET+2] = (data>>16) & 0xFF;
+                mem[addr-ROM_OFFSET+3] = (data>>24) & 0xFF;
+            }else{
+                printf("strange data access addr:%u, data:%u, type:%u\n", addr, data, type);
+            }
+        }else if(addr == SERIAL_PORT){
+            putchar((uint8_t)data);
         }else{
-            printf("strange data access addr:%u, data:%u, type:%u\n", addr, data, type);
+            printf("Illegal memory write access at addr:0x%08x\n",addr);
+            assert(0);
         }
     }
 
-    int memread(uint32_t addr){
-        if(addr<ROM_OFFSET || addr>(ROM_OFFSET + MEM_SIZE)){
+    uint32_t memread(uint32_t addr){
+        if(addr>ROM_OFFSET || addr<(ROM_OFFSET + MEM_SIZE)){
+            return ((mem[addr-ROM_OFFSET+3]<<24)|
+                    (mem[addr-ROM_OFFSET+2]<<16)|
+                    (mem[addr-ROM_OFFSET+1]<<8)|
+                    (mem[addr-ROM_OFFSET]));
+        }else if(addr == RTC_ADDR || addr == RTC_ADDR + 4){
+            if(addr == RTC_ADDR + 4){
+                time(&curr_time);
+                return curr_time>>32;
+            }
+            if(addr == RTC_ADDR) return (uint32_t)curr_time;
+        }else{
             printf("Illegal memory read access at addr:0x%08x\n",addr);
-            return 0;
+            assert(0);
         }
-        return ((mem[addr-ROM_OFFSET+3]<<24)|(mem[addr-ROM_OFFSET+2]<<16)|(mem[addr-ROM_OFFSET+1]<<8)|(mem[addr-ROM_OFFSET]));
     }
 }
