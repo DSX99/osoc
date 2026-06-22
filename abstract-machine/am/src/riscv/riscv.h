4fba047301f72d8424186d66a87e0fea6209c9a3
tracer-ysyx <tracer@ysyx.org>
2026-06-22 07:11:10 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:11:10 up  1:02,  1 user,  load average: 0.11, 0.07, 0.07

--- abstract-machine/am/src/riscv/riscv.h
+++ abstract-machine/am/src/riscv/riscv.h
@@ -33,7 +33,7 @@ enum { MODE_U, MODE_S, MODE_M = 3 };
 
 #endif
 
-#define DEVICE_BASE 0x00000000 // <- all zeros
+#define DEVICE_BASE 0xa0000000
 
 #define SERIAL_PORT     (DEVICE_BASE + 0x00003f8)
 #define KBD_ADDR        (DEVICE_BASE + 0x0000060)
