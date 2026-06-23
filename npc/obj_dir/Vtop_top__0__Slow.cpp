9ec12f8b4b3ed725c4ca7a4967e3381db7a27175
tracer-ysyx <tracer@ysyx.org>
2026-06-23 17:37:12 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:37:12 up 52 min,  1 user,  load average: 0.32, 0.23, 0.18

--- npc/obj_dir/Vtop_top__0__Slow.cpp
+++ npc/obj_dir/Vtop_top__0__Slow.cpp
@@ -12,7 +12,6 @@ VL_ATTR_COLD void Vtop_top___eval_initial__TOP__top(Vtop_top* vlSelf) {
     vlSelfRef.pc = 0x80000000U;
     vlSelfRef.__PVT__csr_mod__DOT__regs[0U] = 0U;
     vlSelfRef.__PVT__csr_mod__DOT__regs[1U] = 0U;
-    vlSelfRef.__PVT__csr_mod__DOT__regs[2U] = 0U;
     vlSelfRef.__PVT__csr_mod__DOT__regs[3U] = 0U;
     vlSelfRef.__PVT__csr_mod__DOT__regs[4U] = 0U;
     vlSelfRef.__PVT__csr_mod__DOT__regs[5U] = 0U;
