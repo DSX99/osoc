4dc6ad636e39082f57a685a5c55a62c63091714f
tracer-ysyx <tracer@ysyx.org>
2026-06-23 16:34:38 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  16:34:38 up 10:40,  1 user,  load average: 0.35, 0.21, 0.09

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -44,7 +44,7 @@ extern "C" {
 
         printf("The image is %s, size = %ld\n", img_file, size);
 
-        printf("1");
+        printf("1\n");
 
         fseek(fp, 0, SEEK_SET);
         int ret = fread(mem, size, 1, fp);
@@ -52,13 +52,13 @@ extern "C" {
 
         fclose(fp);
 
-        printf("1");
+        printf("1\n");
 
         if(!batch){
             difftest_memcpy(0x80000000, mem, size, 1);
         }
 
-        printf("2");
+        printf("1\n");
     }
 
     void memwrite(uint32_t addr, uint32_t data, uint32_t type){
