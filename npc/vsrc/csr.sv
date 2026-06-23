fa9da427cbd6578f1a47d448f573994870a3c7c2
tracer-ysyx <tracer@ysyx.org>
2026-06-23 13:01:27 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:01:27 up  7:06,  1 user,  load average: 0.06, 0.05, 0.03

---
+++ npc/vsrc/csr.sv
@@ -0,0 +1,59 @@
+module csr(
+    input logic clk,
+    input logic rst,
+    input logic [1:0] oper,
+    input logic [4:0] cause,
+    input logic [11:0] addr,
+    input logic [31:0] data_in,
+    input logic [31:0] pc,
+    output logic [31:0] data_out
+);
+
+logic [31:0] regs [3:0];
+
+enum{
+    MEPS, MSTATUS, MCAUSE, MTVEC
+};
+
+logic [3:0] working_reg;
+
+initial begin
+    for(int i=0;i<4;i++) begin
+        regs[i]<=0;
+    end
+end
+
+always_comb begin
+    case(addr)
+        12'h300: working_reg = MSTATUS;
+        12'h305: working_reg = MTVEC;
+        12'h341: working_reg = MEPS;
+        12'h342: working_reg = MCAUSE;
+    endcase
+    if(cause != 0) working_reg = MTVEC;
+    data_out = regs[working_reg];
+end
+
+always_ff @(posedge clk) begin
+    case(oper)
+        2'b00: ;
+        2'b00 : begin
+            regs[working_reg] <= data_in;
+        end
+        2'b10 : begin
+            regs[working_reg] <= regs[working_reg] | data_in;
+        end
+        2'b11 : begin
+            regs[working_reg] <= regs[working_reg] & (~data_in);
+        end
+    endcase
+
+    if(cause !=0) begin
+        regs[MEPS]<=pc;
+        regs[MCAUSE]<=cause;
+    end
+end
+
+
+
+endmodule
