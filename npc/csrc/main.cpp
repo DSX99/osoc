adbb17afb750ef7d92c4cb401102a4b9177dd4e2
tracer-ysyx <tracer@ysyx.org>
2026-06-23 17:32:48 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:32:48 up 48 min,  1 user,  load average: 0.04, 0.16, 0.15

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -160,14 +160,16 @@ void execute(uint32_t n){
       printf("time:%lu\n", contextp->time());
     }
 
-    if(!batch & n<10){
+    if(!batch){
       inst[0] = (top->top->opcode) & 0xff;
       inst[1] = (top->top->opcode >> 8) & 0xff;
       inst[2] = (top->top->opcode >> 16) & 0xff;
       inst[3] = (top->top->opcode >> 24) & 0xff;
-      printf("0x%08x: %02x %02x %02x %02x ", top->top->pc, inst[3], inst[2], inst[1], inst[0]);
-      disassemble(str, 128, top->top->pc, inst, 4);
-      printf("%s\n", str);
+      if(n<10){
+        printf("0x%08x: %02x %02x %02x %02x ", top->top->pc, inst[3], inst[2], inst[1], inst[0]);
+        disassemble(str, 128, top->top->pc, inst, 4);
+        printf("%s\n", str);
+      }
       #ifdef ITRACE
       strcpy(itrace[point],str);
       point = (point+1)%ITRACE_VAL;
