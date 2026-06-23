c9353b62cfa7642e95d29c2799b6614624b4696d
tracer-ysyx <tracer@ysyx.org>
2026-06-23 05:59:03 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  05:59:03 up 4 min,  1 user,  load average: 0.10, 0.13, 0.06

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -57,14 +57,20 @@ void itoa(char *s, int x){
   }
 }
 
+uint32_t addr = 0;
+
 void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+//   // On native, malloc() will be called during initializaion of C runtime.
+//   // Therefore do not call panic() here, else it will yield a dead recursion:
+//   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+//   panic("Not implemented");
+// #endif
+//   return NULL;
+  if(addr == 0) addr = (uint32_t) heap.start;
+  uint32_t temp = addr;
+  addr = addr + (uint32_t)size;
+  return (void *)temp;
 }
 
 void free(void *ptr) {
