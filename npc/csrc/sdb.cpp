9743dad8e358b38bd551160e07b8fe0c4d71371c
tracer-ysyx <tracer@ysyx.org>
2026-06-23 14:51:34 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:51:34 up  8:56,  1 user,  load average: 0.42, 0.21, 0.12

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -180,6 +180,13 @@ static int cmd_sir(char *args) {
 #include "Vtop_top.h"
 #include "Vtop_regs.h"
 
+const char *regs[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 void difftest_regcpy(void *regs, bool direction);
 extern Vtop* top; 
 uint32_t ref_regs[32];
