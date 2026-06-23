47feebe611a8120a98edca431b4b506f24a33b8d
tracer-ysyx <tracer@ysyx.org>
2026-06-23 16:30:48 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  16:30:48 up 10:36,  1 user,  load average: 0.12, 0.11, 0.04

---
+++ npc/obj_dir/Vtop__Dpi.h
@@ -0,0 +1,28 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VTOP__DPI_H_
+#define VERILATED_VTOP__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at vsrc/lsu.sv:21:33
+    extern int memread(int addr);
+    // DPI import at vsrc/lsu.sv:20:34
+    extern void memwrite(int addr, int data, int idk);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
