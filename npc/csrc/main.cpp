c1e0043d8c993c1b04ad7ee606cacdcbd91c6df0
tracer-ysyx <tracer@ysyx.org>
2026-06-23 14:06:34 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:06:34 up  8:11,  1 user,  load average: 0.29, 0.21, 0.11

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -153,7 +153,7 @@ void execute(uint32_t n){
       printf("%s\n", str);
     }
 
-    if(!batch) difftest_exec(1);
+    // if(!batch) difftest_exec(1);
 
     contextp->timeInc(1);
     top->clk=!top->clk;
