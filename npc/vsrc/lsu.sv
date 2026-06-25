1b5dbc7fca77c244c79461cd02738ff496a4c0fc
tracer-ysyx <tracer@ysyx.org>
2026-06-25 18:42:41 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:42:41 up  1:39,  1 user,  load average: 0.18, 0.10, 0.08

--- npc/vsrc/lsu.sv
+++ npc/vsrc/lsu.sv
@@ -61,8 +61,8 @@ module lsu(
     logic prev_le;
 
     always_comb begin
-        valid_right = valid_left && !(!prev_le && bus_in.lsu_le && ready);
-        ready_left = ready_right && !(!prev_le && bus_in.lsu_le && ready);
+        valid_right = valid_left && !(!prev_le && bus_in.lsu_le) && ready;
+        ready_left = ready_right && !(!prev_le && bus_in.lsu_le) && ready;
         unused_branch = bus_in.branch | |rresp | |bresp;
 
         bus_out.alu_out = 0;
