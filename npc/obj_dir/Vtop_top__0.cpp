80713a667b49dcfc77bee4d0b67b6acd574822c6
tracer-ysyx <tracer@ysyx.org>
2026-06-25 16:24:20 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  16:24:20 up 13 min,  1 user,  load average: 0.28, 0.22, 0.12

--- npc/obj_dir/Vtop_top__0.cpp
+++ npc/obj_dir/Vtop_top__0.cpp
@@ -1209,11 +1209,11 @@ void Vtop_top___nba_sequent__TOP__top__0(Vtop_top* vlSelf) {
             __Vdly__ifu_mod__DOT__ifu = 1U;
             vlSelfRef.__Vdly__if_id_valid = 0U;
         } else if ((1U == vlSelfRef.__PVT__ifu_mod__DOT__ifu)) {
+            vlSelfRef.__Vdly__if_id_valid = 0U;
             if (vlSelfRef.__PVT__arready_ifu) {
                 vlSelfRef.__PVT__arvalid_ifu = 0U;
                 __Vdly__ifu_mod__DOT__ifu = 2U;
                 vlSelfRef.__PVT__rready_ifu = 1U;
-                vlSelfRef.__Vdly__if_id_valid = 0U;
             }
         } else if ((2U == vlSelfRef.__PVT__ifu_mod__DOT__ifu)) {
             if (vlSelfRef.__PVT__rvalid_ifu) {
