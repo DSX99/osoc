8425bd05182f661e87b8549a1654af192e80c862
tracer-ysyx <tracer@ysyx.org>
2026-06-25 19:03:10 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  19:03:10 up  2:00,  1 user,  load average: 0.12, 0.19, 0.15

--- npc/vsrc/ifu.sv
+++ npc/vsrc/ifu.sv
@@ -68,7 +68,7 @@ always_ff @(posedge clk) begin
                 end
                 AWAIT: begin
                     ifu<=IDLE;
-                    valid<=1;
+                    valid<=0;
                 end
             endcase
         end
