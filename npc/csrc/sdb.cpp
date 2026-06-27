35fca5bb771613f5d172c03f1cf15b3b4ead9c51
tracer-ysyx <tracer@ysyx.org>
2026-06-27 13:14:31 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:14:31 up  6:58,  1 user,  load average: 0.12, 0.15, 0.10

--- npc/csrc/sdb.cpp
+++ npc/csrc/sdb.cpp
@@ -18,6 +18,7 @@
 #include <cstdlib>
 #include <cstdint>
 #include "dpi.h"
+#include "common.h"
 
 static int is_batch_mode = false;
 static char prev_cmd[128];
@@ -32,6 +33,7 @@ void print_ftrace();
 void reg_display();
 uint32_t expr(char *e, bool *success);
 void execute(uint32_t n);
+VysyxSoCFull_osoc_26000003 *top;
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
@@ -182,14 +184,13 @@ const static char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 extern "C" void difftest_regcpy(void *regs, bool direction);
-extern Vtop* top; 
 uint32_t ref_regs[32];
 
 static int cmd_check(char *args) {
 
   difftest_regcpy(ref_regs, 0);
   for(int i=0;i<32;i++){
-    if(ref_regs[i]-top->top->reg_mod->regs[i]!=0){
+    if(ref_regs[i]-top->reg_mod->regs[i]!=0){
       printf("Difference with REF %s, should:0x%08x, actually:0x%08x, pc: 0x%08x\n", regs[i], ref_regs[i], top->top->reg_mod->regs[i], top->top->pc);
       return 0;
     }
