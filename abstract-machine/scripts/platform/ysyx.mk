4ee8c51b1c5032101e299e8394f5a48d1619f762
tracer-ysyx <tracer@ysyx.org>
2026-06-28 13:18:34 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:18:34 up  1:36,  1 user,  load average: 0.08, 0.06, 0.03

--- abstract-machine/scripts/platform/ysyx.mk
+++ abstract-machine/scripts/platform/ysyx.mk
@@ -10,8 +10,8 @@ AM_SRCS := riscv/ysyxSoC/start.S \
 
 CFLAGS    += -fdata-sections -ffunction-sections
 CFLAGS    += -I$(AM_HOME)/am/src/riscv/
-LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
-LDFLAGS   += --defsym=_pmem_start=0x20000000 --defsym=_entry_offset=0x0
+LDSCRIPTS += $(AM_HOME)/scripts/ysyxlinker.ld
+LDFLAGS   += --defsym=_pmem_start=0x0f000000 --defsym=_entry_offset=0x20000000
 LDFLAGS   += --gc-sections -e _start
 
 MAINARGS_MAX_LEN = 64
