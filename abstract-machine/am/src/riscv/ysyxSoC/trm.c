06556566ef44fde1ac814df2ec8bdb41095aeb96
tracer-ysyx <tracer@ysyx.org>
2026-06-29 12:44:33 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:44:33 up 34 min,  1 user,  load average: 0.20, 0.21, 0.19

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -18,7 +18,7 @@ static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); /
 #define UART_IER  1   // Interrupt Enable Register / DLM (RW when DLAB=1)
 #define UART_FCR  2   // FIFO Control Register (W)
 #define UART_LC   3   // Line Control Register (RW)
-#define UART_LS   4
+#define UART_LS   5   // Line Status Register
 
 void putch(char ch) {
   volatile char *data = (char *)(UART_BASE);
