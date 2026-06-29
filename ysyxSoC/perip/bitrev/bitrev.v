5937566858e086d51c3963a0957a034782e11e39
dsx99 <dsx99@idk.com>
2026-06-29 19:26:49 +0500

    end of 29/06/2026. Working spi_test of bitrev

--- ysyxSoC/perip/bitrev/bitrev.v
+++ ysyxSoC/perip/bitrev/bitrev.v
@@ -1,8 +1,26 @@
 module bitrev (
-  input  sck,
-  input  ss,
-  input  mosi,
-  output miso
+  input wire sck,
+  input wire ss,
+  input wire mosi,
+  output reg miso
 );
-  assign miso = 1'b1;
+
+  reg [6:0] buff;
+  reg [3:0] count;
+
+  always @(posedge sck) begin
+    miso <= 0;
+    if(~ss) begin
+      if(count<4'd7)begin
+        buff <= {mosi, buff[6:1]};
+      end else if(count==4'd7)begin
+        miso <= mosi;
+      end else begin
+        buff <= {buff[5:0],1'b0};
+        miso <= buff[6];
+      end
+      count<=count+1;
+    end
+  end
+
 endmodule
