d08902e2a89105e78ed412231324950fec1c504b
tracer-ysyx <tracer@ysyx.org>
2026-06-29 14:17:24 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:17:24 up  2:06,  1 user,  load average: 0.49, 0.39, 0.30

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -47,8 +47,8 @@ void _trm_init() {
 *(volatile uint8_t *)(UART_BASE + UART_IER) = 0;      // disable interrupts
 uint16_t divisor = 1;
 *(volatile uint8_t *)(UART_BASE + UART_LC)  = 0x80;   // LCR: set DLAB=1
-*(volatile uint8_t *)(UART_BASE + 0)         = (uint8_t)(divisor & 0xFF);      // DLL
 *(volatile uint8_t *)(UART_BASE + 1)         = (uint8_t)((divisor >> 8) & 0xFF); // DLM (use >>8)
+*(volatile uint8_t *)(UART_BASE + 0)         = (uint8_t)(divisor & 0xFF);      // DLL
 *(volatile uint8_t *)(UART_BASE + UART_LC)  = 0x03;   // LCR: 8N1, clear DLAB
 *(volatile uint8_t *)(UART_BASE + UART_FCR) = 0x07;   // FCR: enable + clear FIFOs (optional)
 *(volatile uint8_t *)(UART_BASE + 4)         = 0x00;  // MCR (optional)
