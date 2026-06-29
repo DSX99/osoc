71c595e629d2da57ce2512ed6ef20f7bf8d60535
tracer-ysyx <tracer@ysyx.org>
2026-06-29 13:10:29 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:10:29 up 59 min,  1 user,  load average: 0.07, 0.15, 0.17

--- npc/vsrc/lsu.sv
+++ npc/vsrc/lsu.sv
@@ -281,7 +281,7 @@ always_ff @(posedge clk) begin
                 IDLE_W:begin
                     done_w<=0;
                     if(bus_in.lsu_we && valid_left) begin
-                        awaddr<=(bus_in.alu_out & 32'hFFFFFFFC);
+                        awaddr<=(bus_in.alu_out & 32'hFFFFFFFF);
                         awvalid<=1;
                         wdata<=(bus_in.data_rs2 << (bus_in.alu_out[1:0]*8));
                         wvalid<=1;
