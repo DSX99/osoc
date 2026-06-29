05eedd06c8ff1a10bff722cdcb1e1ed1eda66005
tracer-ysyx <tracer@ysyx.org>
2026-06-29 13:13:30 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:13:30 up  1:03,  1 user,  load average: 0.39, 0.33, 0.23

--- npc/vsrc/lsu.sv
+++ npc/vsrc/lsu.sv
@@ -236,8 +236,8 @@ LSU_state_w_t lsu_w;
 
 always_comb begin
     case(bus_in.lsu_oper) 
-        3'b000: wstrb=(4'b0001 << bus_in.alu_out[1:0]);
-        3'b001: wstrb=(4'b0011 << bus_in.alu_out[1:0]);
+        3'b000: wstrb=(4'b0001); // << bus_in.alu_out[1:0]
+        3'b001: wstrb=(4'b0011);
         3'b010: wstrb=4'b1111;
         default: wstrb=0;
     endcase
