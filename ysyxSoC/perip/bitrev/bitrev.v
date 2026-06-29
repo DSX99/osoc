fd6dd7681e6342bb702c330fd807f7b44d75e257
tracer-ysyx <tracer@ysyx.org>
2026-06-29 19:01:26 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  19:01:26 up  6:50,  1 user,  load average: 0.00, 0.03, 0.04

--- ysyxSoC/perip/bitrev/bitrev.v
+++ ysyxSoC/perip/bitrev/bitrev.v
@@ -1,8 +1,26 @@
 module bitrev (
-  input  sck,
-  input  ss,
-  input  mosi,
-  output miso
+  input wire sck,
+  input wire ss,
+  input wire mosi,
+  output reg miso
 );
-  assign miso = 1'b1;
+
+  reg buff [6:0];
+  reg count [3:0];
+
+  always @(posedge sck) begin
+    miso <= 1;
+    if(~ss) begin
+      if(count<7)begin
+        buff <= {mosi, buff[6:1]};
+      end else if(count==7)begin
+        miso <= mosi;
+      end else begin
+        buff <= buff{buff[5:1],1'b0};
+        miso <= buff[6];
+      end
+      count<=count+1;
+    end
+  end
+
 endmodule
