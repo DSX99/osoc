cf86d6657bf34849fff755cfb83de8941bef8273
tracer-ysyx <tracer@ysyx.org>
2026-06-29 12:35:42 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:35:42 up 25 min,  1 user,  load average: 0.29, 0.26, 0.20

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -45,7 +45,7 @@ void _trm_init() {
   memcpy(&_data_VMA, &_data_start, (uint32_t)&_data_size);
 
   *(volatile char *)(UART_BASE + UART_LC) = 0b10000011;
-  *(volatile char *)(UART_BASE + UART_IER) = 0x1;
+  *(volatile char *)(UART_BASE + UART_IER) = 0x11;
   *(volatile char *)(UART_BASE + UART_TX)  = 0x11;
   *(volatile char *)(UART_BASE + UART_LC) = 0b00000011;
   *(volatile char *)(UART_BASE + UART_FCR) = 0b00000110;
