4648056ba9d7979b2ae6f78e6865038e745b991c
tracer-ysyx <tracer@ysyx.org>
2026-06-29 13:45:28 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:45:28 up  1:34,  1 user,  load average: 0.02, 0.07, 0.08

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -44,20 +44,21 @@ void _trm_init() {
   }
   memcpy(&_data_VMA, &_data_start, (uint32_t)&_data_size);
 
-  *(volatile char *)(UART_BASE ) = 0b00000000;
-  *(volatile char *)(UART_BASE +1) = 0b00000000;
-  *(volatile char *)(UART_BASE +2) = 0b00000000;
-  *(volatile char *)(UART_BASE +3) = 0b00000000;
-  *(volatile char *)(UART_BASE +4) = 0b00000000;
-  *(volatile char *)(UART_BASE +5) = 0b00000000;
-  *(volatile char *)(UART_BASE +6) = 0b00000000;
-  *(volatile char *)(UART_BASE +7) = 0b00000000;
+  *(volatile uint8_t *)(UART_BASE + UART_IER) = 0;
+  uint16_t divisor = 115200/9600;
+  *(volatile uint8_t *)(UART_BASE + UART_LC) = 0b10000011;
+  *(volatile uint8_t *)(UART_BASE + 0) = (uint8_t)divisor & 0xff;
+  *(volatile uint8_t *)(UART_BASE + 1) = (uint8_t)((divisor & 0xff00)>>2);
+  *(volatile uint8_t *)(UART_BASE + UART_LC) = 0b00000011;
+  
+  *(volatile uint8_t *)(UART_BASE + 4) = 0;
+  *(volatile uint8_t *)(UART_BASE + UART_IER) = 1;
 
-  *(volatile char *)(UART_BASE + UART_LC) = 0b10000011;
-  *(volatile char *)(UART_BASE + UART_IER) = 0x00;
-  *(volatile char *)(UART_BASE + UART_TX)  = 0x01;
-  *(volatile char *)(UART_BASE + UART_LC) = 0b00000011;
+  volatile uint8_t temp;
 
+  temp = *(volatile uint8_t *)(UART_BASE + 3);
+  temp = *(volatile uint8_t *)(UART_BASE + 0);
+  putch(temp);
 
   int ret = main(mainargs);
   halt(ret);
