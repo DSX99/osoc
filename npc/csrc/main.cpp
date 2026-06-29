e2653d768b7ff0ea52343706df3543ef737d3ed4
tracer-ysyx <tracer@ysyx.org>
2026-06-29 06:03:05 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  06:03:05 up  6:40,  1 user,  load average: 0.18, 0.11, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -263,6 +263,17 @@ void execute(uint32_t n){
         if (ref_cpu.pc != top->pc) {
           printf("Difference with REF pc, should:0x%08x, actually:0x%08x\n", ref_cpu.pc, top->pc);
           ret = 1;
+          inst[0] = (top->opcode) & 0xff;
+          inst[1] = (top->opcode >> 8) & 0xff;
+          inst[2] = (top->opcode >> 16) & 0xff;
+          inst[3] = (top->opcode >> 24) & 0xff;
+          printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
+          disassemble(str, 128, top->pc, inst, 4);
+          printf("%s\n", str);
+          #ifdef ITRACE
+          strcpy(itrace[point],str);
+          point = (point+1)%ITRACE_VAL;
+          #endif
           return; 
         }
 
@@ -271,6 +282,17 @@ void execute(uint32_t n){
             printf("Difference with REF %s, should:0x%08x, actually:0x%08x, pc: 0x%08x\n", 
                    regs[i], ref_cpu.gpr[i], top->reg_mod->regs[i], top->pc);
             ret = 1;
+            inst[0] = (top->opcode) & 0xff;
+            inst[1] = (top->opcode >> 8) & 0xff;
+            inst[2] = (top->opcode >> 16) & 0xff;
+            inst[3] = (top->opcode >> 24) & 0xff;
+            printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
+            disassemble(str, 128, top->pc, inst, 4);
+            printf("%s\n", str);
+            #ifdef ITRACE
+            strcpy(itrace[point],str);
+            point = (point+1)%ITRACE_VAL;
+            #endif
             return;
           }
         }
