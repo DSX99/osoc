84296919b556f8fb823431bc9f88aca3cbf309ab
tracer-ysyx <tracer@ysyx.org>
2026-06-30 14:54:25 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.1-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Fri Jun  5 01:12:21 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:54:25 up 20:25,  1 user,  load average: 0.32, 0.18, 0.09

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -42,7 +42,7 @@ uint8_t flash[] = {
     0x23,0x00,0xf7,0x00
 };
 
-extern "C" void flash_read(uint32_t addr, uint32_t *data) { addr = addr & 0xfffffffc; *data = ((flash[addr+3]<<24)|(flash[addr+2]<<16)|(flash[addr+1]<<8)|(flash[addr])); }
+extern "C" void flash_read(uint32_t addr, uint32_t *data) { addr = addr & 0xfffffffc; *data = ((flash[addr+3])|(flash[addr+2]<<8)|(flash[addr+1]<<16)|(flash[addr]<<24)); }
 extern "C" void mrom_read(uint32_t addr, uint32_t *data) { addr = addr & 0xfffffffc; *data = ((mem[addr-MROM_OFFSET+3]<<24)|(mem[addr-MROM_OFFSET+2]<<16)|(mem[addr-MROM_OFFSET+1]<<8)|(mem[addr-MROM_OFFSET])); }
 
 extern "C" {
