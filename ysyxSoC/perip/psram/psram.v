228464787a5671f6badeaadda13a2056e4e16080
tracer-ysyx <tracer@ysyx.org>
2026-07-01 18:48:49 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:48:49 up  5:12,  1 user,  load average: 0.39, 0.21, 0.20

--- ysyxSoC/perip/psram/psram.v
+++ ysyxSoC/perip/psram/psram.v
@@ -20,11 +20,11 @@ module psram(
     else counter<=counter+1;
 
     if(counter==8) begin
-      if(oper == 8'h38) rw<=1;
+      if(oper == 8'h35) set<=1;
     end 
     if (counter==2) begin
       if(oper == 8'heb) rw<=0;
-      else if(oper == 8'h35) set<=1;
+      else if(oper == 8'h38) rw<=1;
       else begin
         $display("Wrong code for psram %x",oper);
         $finish;
