e9344e8639184d40af7f152ef82418d177076e8f
tracer-ysyx <tracer@ysyx.org>
2026-07-01 06:16:30 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  06:16:30 up 15 min,  1 user,  load average: 0.06, 0.24, 0.20

--- ysyxSoC/perip/spi/rtl/spi_top_apb.v
+++ ysyxSoC/perip/spi/rtl/spi_top_apb.v
@@ -156,9 +156,10 @@ always @(posedge clock) begin
           pwdata<=32'h00002140;
           if(out_pready && out_prdata==32'h00002040) begin
             fsm_state<=7;
+            pwdata<=32'h0;
+            prdata<=out_prdata; 
             psel<=0;
             penable<=0;
-            prdata<=out_prdata;
             pready<=1;
             pslverr<=out_pslverr;
           end else if(out_pready) begin
@@ -168,7 +169,10 @@ always @(posedge clock) begin
         3'd6:begin
           if(out_prdata==32'h00002040) begin
             fsm_state<=7;
+            pwdata<=32'h0;
             prdata<=out_prdata;
+            psel<=0;
+            penable<=0;
             pready<=1;
             pslverr<=out_pslverr;
           end else fsm_state<=5;
