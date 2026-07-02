81b684c517023c86574d52677d515c7beb094835
tracer-ysyx <tracer@ysyx.org>
2026-07-02 17:46:40 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:46:40 up  7:22,  1 user,  load average: 0.38, 0.28, 0.20

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -50,7 +50,7 @@ extern "C" void psram_read(uint32_t addr, uint32_t *data) {
     *data=(psram[addr]);
 }
 extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
-    printf("Call to write to addr:0x%x ,data:0x%04x ,half:%x\n",addr, data, mask);
+    // printf("Call to write to addr:0x%x ,data:0x%04x ,half:%x\n",addr, data, mask);
     if (!(mask & 0x01)) sdram[addr] = data & 0xFF;
     if (!(mask & 0x02)) sdram[addr + 1] = (data >> 8) & 0xFF;
 
@@ -59,7 +59,7 @@ extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
     // }
 }
 extern "C" void sdram_read(uint32_t addr, uint32_t *data) {
-    printf("Call to read from addr:0x%x\n",addr);
+    // printf("Call to read from addr:0x%x\n",addr);
     *data = sdram[addr] | (sdram[addr + 1] << 8);
 }
 
