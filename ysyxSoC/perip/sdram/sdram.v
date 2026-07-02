42b19c86d88e39bb3ad4bd22a7d54c35e2be63d1
tracer-ysyx <tracer@ysyx.org>
2026-07-02 17:45:49 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:45:49 up  7:21,  1 user,  load average: 0.27, 0.24, 0.18

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -134,12 +134,14 @@ import "DPI-C" function void sdram_read(input int addr, output int data);
 
         if(count==1 & burst_read==0) sdram_write({7'b0, addr[23:9], addr[8:0], 1'b0} + 32'd2, {16'b0, dq}, {30'b0, dqm});
 
-        if(count>={7'b0,cas_lat} & burst_read==1) sdram_read({7'b0, addr[23:0], 1'b0} + ({22'b0, count} - {29'b0, cas_lat}) * 32'd2, {16'b0,buff});
-
         if(count=={7'b0,cas_lat}+burst_len-1) burst_read<=0;
 
       end
     end
+  end
+
+  always @* begin
+            if(count>={7'b0,cas_lat} & burst_read==1) sdram_read({7'b0, addr[23:0], 1'b0} + ({22'b0, count} - {29'b0, cas_lat}) * 32'd2, {16'b0,buff});
   end
 
   assign dq = (count>={7'b0,cas_lat} & burst_read==1) ? buff : 16'bz;
