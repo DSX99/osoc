73f3d9784621b2d2093d885d005178d24b2867dd
tracer-ysyx <tracer@ysyx.org>
2026-07-02 17:33:05 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:33:05 up  7:09,  1 user,  load average: 0.12, 0.17, 0.13

--- ysyxSoC/perip/sdram/sdram.v
+++ ysyxSoC/perip/sdram/sdram.v
@@ -132,7 +132,7 @@ import "DPI-C" function void sdram_read(input int addr, output int data);
 
         if(will_stop_burst) burst_read<=0;
 
-        if(count==1 & burst_read==0) sdram_write({6'b0,addr[25:11],addr[10:2],2'b0},{16'b0,dq}, {30'b0,dqm});
+        if(count==1 & burst_read==0) sdram_write({6'b0,addr[25:11],addr[10:2],1'b1,1'b0},{16'b0,dq}, {30'b0,dqm});
 
         if(count>={7'b0,cas_lat} & burst_read==1) sdram_read({6'b0,addr+{16'b0,count-{7'b0,cas_lat}}},{16'b0,buff});
 
