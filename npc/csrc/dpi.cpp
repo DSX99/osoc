f27958b97e35233e84b328bc9d4d7be3850cd647
tracer-ysyx <tracer@ysyx.org>
2026-07-03 07:33:35 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:33:34 up  1:32,  1 user,  load average: 0.25, 0.12, 0.10

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -54,9 +54,9 @@ extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
     if (!(mask & 0x01)) sdram[addr] = data & 0xFF;
     if (!(mask & 0x02)) sdram[addr + 1] = (data >> 8) & 0xFF;
 
-    for(int i=0;i<10;i++){
-        printf("i:%d,mem:%02x\n",i,sdram[i]);
-    }
+    // for(int i=0;i<10;i++){
+    //     printf("i:%d,mem:%02x\n",i,sdram[i]);
+    // }
 }
 extern "C" void sdram_read(uint32_t addr, uint32_t *data) {
     // printf("Call to read from addr:0x%x\n",addr);
