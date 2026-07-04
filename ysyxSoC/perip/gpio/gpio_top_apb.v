7b29dc8e7b097d872381d70ba5f6754e0f4e56ba
tracer-ysyx <tracer@ysyx.org>
2026-07-04 07:36:43 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:36:43 up 0 min,  1 user,  load average: 0.37, 0.14, 0.05

--- ysyxSoC/perip/gpio/gpio_top_apb.v
+++ ysyxSoC/perip/gpio/gpio_top_apb.v
@@ -24,4 +24,49 @@ module gpio_top_apb(
   output [7:0]  gpio_seg_7
 );
 
+reg [15:0] led;
+
+reg [7:0] hex [7:0];
+
+assign gpio_out = led;
+assign gpio_seg_0 = hex[0];
+assign gpio_seg_1 = hex[1];
+assign gpio_seg_2 = hex[2];
+assign gpio_seg_3 = hex[3];
+assign gpio_seg_4 = hex[4];
+assign gpio_seg_5 = hex[5];
+assign gpio_seg_6 = hex[6];
+assign gpio_seg_7 = hex[7];
+
+assign in_prdata = (in_paddr[3:0] == 4'h4) ? {16'b0,gpio_in} : 32'h0;
+assign in_pslverr = 0;
+assign in_pready = (in_psel && in_penable) ? 1'b1 : 1'b0;
+
+always @(posedge clock) begin
+  if(reset) begin
+    for (int i = 0; i < 8; i = i + 1) begin
+      hex[i] <= 0;
+    end
+    led[15:0] <= 0;
+  end else begin
+    if(in_psel && in_penable) begin
+      if(in_pwrite) begin
+        if(in_paddr[3:0] == 4'h0) led <= in_pwdata[15:0];
+        if(in_paddr[3:0] == 4'h8) begin
+          for(int i = 0; i < 4; i = i + 1)begin
+            hex[i] <= in_pwdata[(8*i)+7:(8*i)];
+          end
+        end
+        if(in_paddr[3:0] == 4'hc) begin
+          for(int i = 0; i < 4; i = i + 1)begin
+            hex[4+i] <= in_pwdata[(8*i)+7:(8*i)];
+          end
+        end
+      end
+    end
+  end
+end
+
+
+
 endmodule
