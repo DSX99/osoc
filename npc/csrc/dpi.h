ea1ebcf3cdd56f94c91d319387bc645e2ae3b2f4
tracer-ysyx <tracer@ysyx.org>
2026-07-05 18:43:25 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:43:25 up  4:24,  1 user,  load average: 0.44, 0.37, 0.27

--- npc/csrc/dpi.h
+++ npc/csrc/dpi.h
@@ -19,7 +19,7 @@ typedef struct Performance_t {
 
 void loadmemory(char *img_file, bool batch);
 void init_disasm();
-int memread(uint32_t addr);
+uint32_t memread(uint32_t addr);
 void memwrite(uint32_t addr, uint32_t data, uint32_t type);
 void print_stage_performance_table(const uint64_t cycles[3], const Performance_t perf[3]);
 
