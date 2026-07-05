d63accbe6b057fc609486757b1950cff8899c467
tracer-ysyx <tracer@ysyx.org>
2026-07-05 08:57:06 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  08:57:06 up  3:13,  1 user,  load average: 0.26, 0.26, 0.17

--- npc/vsrc/pc.sv
+++ npc/vsrc/pc.sv
@@ -10,7 +10,7 @@ module pc(
 );
 
     initial begin
-        pc = 32'h80000000;
+        pc = 32'h30000000;
     end
 
     assign next_pc = pc + 4;
@@ -18,7 +18,7 @@ module pc(
 
     always_ff @(posedge clk) begin
         if(rst) begin
-            pc<=32'h80000000;
+            pc<=32'h30000000;
         end else begin
             if(valid) begin
                 pc<=next_pc;
