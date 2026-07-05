2d5c10d6d3c6a49e384494edccfe14994aec3099
tracer-ysyx <tracer@ysyx.org>
2026-07-05 07:35:53 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:35:53 up  1:52,  1 user,  load average: 0.37, 0.18, 0.12

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -189,10 +189,10 @@ extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
     extern "C" uint32_t memread(uint32_t addr){
         printf("\n\033[034mCall to read from memory at %08x\033[0m\n", addr);
         if(addr>=ROM_OFFSET && addr<(ROM_OFFSET + MEM_SIZE)){
-            return ((mem[addr-MEM_SIZE+3]<<24)|
-            (mem[addr-MEM_SIZE+2]<<16)|
-            (mem[addr-MEM_SIZE+1]<<8)|
-            (mem[addr-MEM_SIZE]));
+            return ((mem[addr-ROM_OFFSET+3]<<24)|
+            (mem[addr-ROM_OFFSET+2]<<16)|
+            (mem[addr-ROM_OFFSET+1]<<8)|
+            (mem[addr-ROM_OFFSET]));
         }{
             printf("Illegal memory read access at addr:0x%08x at pc: 0x%08x\n",addr, top->pc);
             fail=1;
