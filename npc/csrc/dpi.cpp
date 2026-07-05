0a110addd449a28daf5a189b791b23fceb68da36
tracer-ysyx <tracer@ysyx.org>
2026-07-05 07:38:04 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:38:04 up  1:54,  1 user,  load average: 0.19, 0.17, 0.12

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -161,7 +161,7 @@ extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
     }
 
     extern "C" void memwrite(uint32_t addr, uint32_t data, uint32_t type){
-        printf("\033[034mCall to write to memory at %08x\033[0m\n", addr);
+        // printf("\033[034mCall to write to memory at %08x\033[0m\n", addr);
         if(addr>=ROM_OFFSET && addr<(ROM_OFFSET + FLASH_SIZE)){
             if (type & 0x1) mem[addr - ROM_OFFSET]     = data & 0xFF;
             if (type & 0x2) mem[addr - ROM_OFFSET + 1] = (data >> 8) & 0xFF;
@@ -178,7 +178,7 @@ extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
     }
 
     extern "C" uint32_t memread(uint32_t addr){
-        printf("\n\033[034mCall to read from memory at %08x\033[0m\n", addr);
+        // printf("\n\033[034mCall to read from memory at %08x\033[0m\n", addr);
         if(addr>=ROM_OFFSET && addr<(ROM_OFFSET + MEM_SIZE)){
             return ((mem[addr-ROM_OFFSET+3]<<24)|
             (mem[addr-ROM_OFFSET+2]<<16)|
