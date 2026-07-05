d5945527bb35d78b7c3f4d41f7252b47ebc222b8
tracer-ysyx <tracer@ysyx.org>
2026-07-05 18:53:02 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:53:02 up  4:33,  1 user,  load average: 0.24, 0.27, 0.26

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -245,6 +245,8 @@ void execute(uint64_t n){
     }
     if(top->ex_ls_valid && !top->ex_ls_ready){
       program[stage].ifu_stall_cycle++;
+    }
+    if(top->ex_ls_valid && top->ex_ls_ready){
       if(top->ex_ls_bus_lsu_we){
         program[stage].lsu_read_data++;
       }
