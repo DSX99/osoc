285a3fee2b3d40e9d29216096ae281ddcb04d9d2
tracer-ysyx <tracer@ysyx.org>
2026-07-05 07:37:25 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:37:25 up  1:54,  1 user,  load average: 0.20, 0.16, 0.12

--- npc/csrc/dpi.cpp
+++ npc/csrc/dpi.cpp
@@ -163,19 +163,10 @@ extern "C" void sdram_write(uint32_t addr, uint32_t data, uint32_t mask) {
     extern "C" void memwrite(uint32_t addr, uint32_t data, uint32_t type){
         printf("\033[034mCall to write to memory at %08x\033[0m\n", addr);
         if(addr>=ROM_OFFSET && addr<(ROM_OFFSET + FLASH_SIZE)){
-            if(type ==0){
-                mem[addr-ROM_OFFSET] = data & 0xFF;
-            }else if (type ==1){
-                mem[addr-ROM_OFFSET] = data & 0xFF;
-                mem[addr-ROM_OFFSET+1] = (data>>8) & 0xFF;
-            }else if (type ==2){
-                mem[addr-ROM_OFFSET] = data & 0xFF;
-                mem[addr-ROM_OFFSET+1] = (data>>8) & 0xFF;
-                mem[addr-ROM_OFFSET+2] = (data>>16) & 0xFF;
-                mem[addr-ROM_OFFSET+3] = (data>>24) & 0xFF;
-            }else{
-                printf("strange data access addr:%u, data:%u, type:%u\n", addr, data, type);
-            }
+            if (type & 0x1) mem[addr - ROM_OFFSET]     = data & 0xFF;
+            if (type & 0x2) mem[addr - ROM_OFFSET + 1] = (data >> 8) & 0xFF;
+            if (type & 0x4) mem[addr - ROM_OFFSET + 2] = (data >> 16) & 0xFF;
+            if (type & 0x8) mem[addr - ROM_OFFSET + 3] = (data >> 24) & 0xFF;
         }else if(addr == SERIAL_PORT){
             skip_inst = 1;
             putchar((uint8_t)data);
