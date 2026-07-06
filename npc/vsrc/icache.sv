da9fd160c3571c39b6cf9b9659329e7d3c289044
tracer-ysyx <tracer@ysyx.org>
2026-07-06 12:15:08 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:15:08 up 13:01,  1 user,  load average: 0.20, 0.16, 0.10

--- npc/vsrc/icache.sv
+++ npc/vsrc/icache.sv
@@ -22,8 +22,8 @@ module icache(
 parameter BLOCK_SIZE = 4;
 parameter NUMBER_OF_BLOCKS = 16;
 
-const int off = $clog2(BLOCK_SIZE);
-const int index_off = $clog2(NUMBER_OF_BLOCKS);
+Constant int off = $clog2(BLOCK_SIZE);
+Constant int index_off = $clog2(NUMBER_OF_BLOCKS);
 
 logic [31:0] block_cache [NUMBER_OF_BLOCKS];
 
