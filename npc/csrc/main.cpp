2290349da1ea7b35e991a909f89784acb88404d7
tracer-ysyx <tracer@ysyx.org>
2026-07-06 13:18:00 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:18:00 up 14:04,  1 user,  load average: 0.43, 0.38, 0.26

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -499,11 +499,10 @@ void print_stage_performance_table(const uint64_t cycles[3], const Performance_t
                 avg_miss_latency[0], avg_miss_latency[1], avg_miss_latency[2]);
     double AMAT = avg_miss_latency[2] * cache_miss_pct[2]/100;
     std::printf("=========================================================================================================\n");
-    std::snprintf(buf0, sizeof(buf0), "%3.1f%%", avg_miss_latency[0] * cache_miss_pct[0]/100);
-    std::snprintf(buf1, sizeof(buf1), "%3.1f%%", avg_miss_latency[1] * cache_miss_pct[1]/100);
-    std::snprintf(buf2, sizeof(buf2), "%3.1f%%", avg_miss_latency[2] * cache_miss_pct[2]/100);
-    std::printf(" %-36s | %-20s | %-20s | %-20s \n", "AMAT", 
-                buf0, buf1, buf2);
+    std::snprintf(buf0, sizeof(buf0), "%3.1f", avg_miss_latency[0] * cache_miss_pct[0]/100);
+    std::snprintf(buf1, sizeof(buf1), "%3.1f", avg_miss_latency[1] * cache_miss_pct[1]/100);
+    std::snprintf(buf2, sizeof(buf2), "%3.1f", avg_miss_latency[2] * cache_miss_pct[2]/100);
+    std::printf(" %-36s | %-20s | %-20s | %-20s \n", "AMAT", buf0, buf1, buf2);
     std::printf("=========================================================================================================\n\n");
               
 }
