87ee2237781210f795d63abbe8443b0162e27349
tracer-ysyx <tracer@ysyx.org>
2026-07-06 13:15:58 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:15:58 up 14:02,  1 user,  load average: 0.69, 0.38, 0.25

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -499,7 +499,7 @@ void print_stage_performance_table(const uint64_t cycles[3], const Performance_t
                 avg_miss_latency[0], avg_miss_latency[1], avg_miss_latency[2]);
     double AMAT = avg_miss_latency[2] * cache_miss_pct[2]/100;
     std::printf("=========================================================================================================\n");
-    std::printf(" %-36s | %3.3f%% | %3.3f%% | %3.3f%% \n", "AMAT", 
+    std::printf(" %-36s | %-203.3f%% | %-203.3f%% | %-203.3f%% \n", "AMAT", 
                 avg_miss_latency[0] * cache_miss_pct[0]/100, avg_miss_latency[1] * cache_miss_pct[1]/100, avg_miss_latency[2] * cache_miss_pct[2]/100);
     std::printf("=========================================================================================================\n\n");
               
