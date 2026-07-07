1e392137c3a44bf29863790c6d46565d1a130f53
tracer-ysyx <tracer@ysyx.org>
2026-07-07 15:28:30 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  15:28:30 up 1 day,  6:23,  1 user,  load average: 0.12, 0.18, 0.17

--- ysyxSoC/perip/sdram/sdram_top_axi.v
+++ ysyxSoC/perip/sdram/sdram_top_axi.v
@@ -37,10 +37,10 @@ module sdram_top_axi(
   output        sdram_ras,
   output        sdram_cas,
   output        sdram_we,
-  output [12:0] sdram_a,
+  output [13:0] sdram_a,
   output [ 1:0] sdram_ba,
-  output [ 1:0] sdram_dqm,
-  inout  [15:0] sdram_dq
+  output [ 3:0] sdram_dqm,
+  inout  [31:0] sdram_dq
 );
 
   wire sdram_dout_en;
