49383f099ce785c26581dd7389449ae54f153c67
tracer-ysyx <tracer@ysyx.org>
2026-07-07 13:25:29 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  13:25:29 up 1 day,  4:20,  1 user,  load average: 0.02, 0.06, 0.08

--- ysyxSoC/perip/amba/apb_delayer.v
+++ ysyxSoC/perip/amba/apb_delayer.v
@@ -25,58 +25,14 @@ module apb_delayer(
 );
 
   assign out_paddr   = in_paddr;
-  assign out_psel    = buff_pready ? 1'b0 : in_psel;
+  assign out_psel    = in_psel;
   assign out_penable = in_penable;
   assign out_pprot   = in_pprot;
   assign out_pwrite  = in_pwrite;
   assign out_pwdata  = in_pwdata;
   assign out_pstrb   = in_pstrb;
-  assign in_pready   = out ? buff_pready : 1'b0;
-  assign in_prdata   = out ? buff_prdata : 32'b0;
-  assign in_pslverr  = out ? buff_pslverr : 1'b0;
-
-  reg        buff_pready;
-  reg [31:0] buff_prdata;
-  reg        buff_pslverr;
-
-
-  parameter ADD = 181;
-
-  reg set;
-  reg [9:0] count;
-  reg [19:0] delay;
-  wire out;
-
-  assign out = delay[19:10] > count;
-
-  always @(posedge clock) begin
-    if(reset) begin
-      count<=0;
-      delay<=0;
-      buff_pready<=0;
-      buff_prdata<=0;
-      buff_pslverr<=0;
-      set<=0;
-    end else begin
-      if(in_psel && in_penable && !out && !buff_pready) begin
-        count<=count+ 1;
-        delay<=ADD;
-        set<=1;
-      end
-      if(out_pready && set) begin
-        buff_pready<=out_pready;
-        buff_prdata<=out_prdata;
-        buff_pslverr<=out_pslverr;
-      end
-      if(delay!=0) begin
-        delay<=delay+ADD;
-      end
-      if(out) begin
-        count<=0;
-        delay<=0;
-        buff_pready<=0;
-      end
-    end
-  end
+  assign in_pready   = out_pready;
+  assign in_prdata   = out_prdata;
+  assign in_pslverr  = out_pslverr;
 
 endmodule
