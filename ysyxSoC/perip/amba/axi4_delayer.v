c451cb7cd96a10dc19ecf36e068b7c02b5d592e9
tracer-ysyx <tracer@ysyx.org>
2026-07-08 06:35:47 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  06:35:47 up 1 day, 16:00,  1 user,  load average: 0.10, 0.32, 0.40

--- ysyxSoC/perip/amba/axi4_delayer.v
+++ ysyxSoC/perip/amba/axi4_delayer.v
@@ -155,7 +155,7 @@ module axi4_delayer(
         FIFO_r_count <= FIFO_r_count + 1;
       end
       if(out_rlast) set<=0;
-      if(!(out_rvalid && out_rlast) && set)begin
+      if(!out_rlast && set)begin
         count_r<=count_r+1;
       end
       if(delay_r!=0) begin
