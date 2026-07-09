8365119c936eb71053a404eabd2d866a56b5493a
tracer-ysyx <tracer@ysyx.org>
2026-07-09 15:22:04 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  15:22:04 up 2 days, 18:09,  1 user,  load average: 0.16, 0.11, 0.03

--- npc/vsrc/alu.sv
+++ npc/vsrc/alu.sv
@@ -57,8 +57,10 @@ module alu (
     assign branch = bus_in_alu_op[5:4] == 2'b01;
     assign branch_taken = bus_out_branch;
 
-    logic reg_match = (ls_rd == bus_in_rs1) || (ls_rd == bus_in_rs2) || (wb_rd == bus_in_rs1) || (wb_rd == bus_in_rs2);
+    logic ls_match = ((ls_rd == bus_in_rs1) || (ls_rd == bus_in_rs2)) && (ls_rd!=0);
+    logic wb_match = ((wb_rd == bus_in_rs1) || (wb_rd == bus_in_rs2)) && (wb_rd!=0);
 
+    logic reg_match == ls_match || wb_match;
 
     always_comb begin
         valid_right = valid_left & !reg_match;
