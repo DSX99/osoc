aff8d73b33f847a8e6c4445f38939b98518188e0
tracer-ysyx <tracer@ysyx.org>
2026-07-09 07:00:20 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:00:20 up 2 days,  9:47,  1 user,  load average: 0.12, 0.25, 0.26

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -229,7 +229,7 @@ void execute(uint64_t n){
       inst[1] = (top->opcode >> 8) & 0xff;
       inst[2] = (top->opcode >> 16) & 0xff;
       inst[3] = (top->opcode >> 24) & 0xff;
-      disassemble(str, 128, top->pc, inst, 4);
+      // disassemble(str, 128, top->pc, inst, 4);
       if(n<10){
         printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
         printf("%s\n", str);
