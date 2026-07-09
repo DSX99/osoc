1eda4b9033475d3d2ed6311bb2b613724a25be9d
tracer-ysyx <tracer@ysyx.org>
2026-07-09 07:00:50 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  07:00:50 up 2 days,  9:48,  1 user,  load average: 0.36, 0.29, 0.27

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -327,7 +327,7 @@ void execute(uint64_t n){
         inst[2] = (top->opcode >> 16) & 0xff;
         inst[3] = (top->opcode >> 24) & 0xff;
         printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
-        disassemble(str, 128, top->pc, inst, 4);
+        // disassemble(str, 128, top->pc, inst, 4);
         printf("%s\n", str);
         #ifdef ITRACE
         strcpy(itrace[point],str);
@@ -350,7 +350,7 @@ void execute(uint64_t n){
       inst[2] = (top->opcode >> 16) & 0xff;
       inst[3] = (top->opcode >> 24) & 0xff;
       printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
-      disassemble(str, 128, top->pc, inst, 4);
+      // disassemble(str, 128, top->pc, inst, 4);
       printf("%s\n", str);
       #ifdef ITRACE
       strcpy(itrace[point],str);
@@ -371,7 +371,7 @@ void execute(uint64_t n){
           inst[2] = (top->opcode >> 16) & 0xff;
           inst[3] = (top->opcode >> 24) & 0xff;
           printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
-          disassemble(str, 128, top->pc, inst, 4);
+          // disassemble(str, 128, top->pc, inst, 4);
           printf("%s\n", str);
           #ifdef ITRACE
           strcpy(itrace[point],str);
@@ -390,7 +390,7 @@ void execute(uint64_t n){
             inst[2] = (top->opcode >> 16) & 0xff;
             inst[3] = (top->opcode >> 24) & 0xff;
             printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
-            disassemble(str, 128, top->pc, inst, 4);
+            // disassemble(str, 128, top->pc, inst, 4);
             printf("%s\n", str);
             #ifdef ITRACE
             strcpy(itrace[point],str);
