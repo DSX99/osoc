7d7b4e533a14c5dc85e3bd2282527af96895f5fa
tracer-ysyx <tracer@ysyx.org>
2026-07-10 06:21:47 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  06:21:47 up 3 days, 13 min,  1 user,  load average: 0.06, 0.11, 0.08

--- npc/vsrc/osoc_26000003_core.sv
+++ npc/vsrc/osoc_26000003_core.sv
@@ -75,7 +75,7 @@ module osoc_26000003_core (
 
     assign rst = reset;
 
-    assign opcode = opcode_over_wb; 
+    assign opcode = ls_wb_bus_branch_wb ? pc_in : opcode_over_wb; 
 
     logic [31:0] opcode_over_ex, opcode_over_ls, opcode_over_wb;
 
