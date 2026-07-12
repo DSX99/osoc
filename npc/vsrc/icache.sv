91b88fdb4635c5ec9be3d4df6de297a936b871a6
tracer-ysyx <tracer@ysyx.org>
2026-07-12 08:29:16 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  08:29:16 up  3:36,  1 user,  load average: 0.12, 0.06, 0.05

--- npc/vsrc/icache.sv
+++ npc/vsrc/icache.sv
@@ -46,8 +46,11 @@ logic [1:0] word_align;
 
 logic [32-index_off-off-1:0] block_tag [NUMBER_OF_BLOCKS];
 logic block_valid[NUMBER_OF_BLOCKS];
+logic do_burst, burst_reg, burst_addr;
 
+assign burst_addr = ifu_addr >= 32'ha0000000 && ifu_addr < 32'hc0000000;
 assign {tag, index, word_select, word_align} = trans ? miss_addr : ifu_addr;
+assign do_burst = trans ? burst_reg : burst_addr;
 
 typedef enum {
    WAIT_AR, WAIT_R
@@ -75,32 +78,21 @@ always_comb begin
             ready  = 1'b1;
         end else begin
             miss=1;
-            if(ifu_addr >= 32'ha0000000 && ifu_addr < 32'hc0000000)begin
-                case (state)
-                    WAIT_AR: begin
-                        arvalid = 1'b1;
+            case (state)
+                WAIT_AR: begin
+                    arvalid = 1'b1;
+                    if(do_burst)begin
                         arburst = 2'b01;
                         arsize = 3'b010;
                         arlen = 8'd7;
-                        araddr  = {tag, index, fill_count, 2'b00};
                     end
-                    WAIT_R: begin
-                        rready  = 1'b1;
-                    end
-                    default: ;
-                endcase
-            end else begin
-                case (state)
-                    WAIT_AR: begin
-                        arvalid = 1'b1;
-                        araddr  = {tag, index, fill_count, 2'b00};
-                    end
-                    WAIT_R: begin
-                        rready  = 1'b1;
-                    end
-                    default: ;
-                endcase
-            end
+                    araddr  = {tag, index, fill_count, 2'b00};
+                end
+                WAIT_R: begin
+                    rready  = 1'b1;
+                end
+                default: ;
+            endcase
         end
     end
 end
@@ -113,65 +105,35 @@ always_ff @(posedge clk) begin
             block_valid[i] <= 1'b0;
         end
     end else if ((valid && !hit) || trans) begin
-        if(ifu_addr >= 32'ha0000000 && ifu_addr < 32'hc0000000)begin
-            case (state)
-                WAIT_AR: begin
-                    if(arvalid && !trans) begin
-                        trans<=1;
-                        miss_addr<=ifu_addr;
-                    end
-                    if (arready && arvalid) begin
-                        state <= WAIT_R;
+        case (state)
+            WAIT_AR: begin
+                if(arvalid && !trans) begin
+                    trans<=1;
+                    miss_addr<=ifu_addr;
+                end
+                if (arready && arvalid) begin
+                    state <= WAIT_R;
+                end
+            end
+            WAIT_R: begin
+                if (rvalid && rready) begin
+                    if(fill_count==3'b111) begin
+                        block_cache[index][fill_count] <= rdata;
+                        block_tag[index] <= tag;
+                        block_valid[index] <= 1'b1;
+                        fill_count <= 0;
+                        trans<=0;
+                        state <= WAIT_AR;
+                    end else begin
+                        block_cache[index][fill_count] <= rdata;
+                        fill_count <= fill_count + 1;
+                        if(do_burst) state <= WAIT_R;
+                        else state <= WAIT_AR;
                     end
                 end
-                WAIT_R: begin
-                    if (rvalid && rready) begin
-                        if(fill_count==3'b111) begin
-                            block_cache[index][fill_count] <= rdata;
-                            block_tag[index] <= tag;
-                            block_valid[index] <= 1'b1;
-                            fill_count <= 0;
-                            trans<=0;
-                            state <= WAIT_AR;
-                        end else begin
-                            block_cache[index][fill_count] <= rdata;
-                            fill_count <= fill_count + 1;
-                            state <= WAIT_R;
-                        end
-                    end
-                end
-                default: state <= WAIT_AR;
-            endcase
-        end else begin
-            case (state)
-                WAIT_AR: begin
-                    if(arvalid && !trans) begin
-                        trans<=1;
-                        miss_addr<=ifu_addr;
-                    end
-                    if (arready && arvalid) begin
-                        state <= WAIT_R;
-                    end
-                end
-                WAIT_R: begin
-                    if (rvalid && rready) begin
-                        if(fill_count==3'b111) begin
-                            block_cache[index][fill_count] <= rdata;
-                            block_tag[index] <= tag;
-                            block_valid[index] <= 1'b1;
-                            fill_count <= 0;
-                            trans<=0;
-                            state <= WAIT_AR;
-                        end else begin
-                            block_cache[index][fill_count] <= rdata;
-                            fill_count <= fill_count + 1;
-                            state <= WAIT_AR;
-                        end
-                    end
-                end
-                default: state <= WAIT_AR;
-            endcase
-        end
+            end
+            default: state <= WAIT_AR;
+        endcase
     end
 end
 
