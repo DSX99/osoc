8133fe58ba10660515bd2fbb0fe230cc7ecb82eb
tracer-ysyx <tracer@ysyx.org>
2026-07-12 11:27:58 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  11:27:58 up  6:34,  1 user,  load average: 0.34, 0.16, 0.05

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -307,7 +307,7 @@ void execute(uint64_t n){
     }
 
     if((top->__PVT__io_master_araddr == 0x200bff8) || (top->__PVT__io_master_araddr == 0x200bffc)){
-      print("device call\n");
+      printf("device call\n");
       device_access = 1;
     }
 
