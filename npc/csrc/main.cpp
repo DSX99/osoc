9f58f1a304f2a191acd1a89d2d6738565ac1e7fe
tracer-ysyx <tracer@ysyx.org>
2026-07-12 09:27:34 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  09:27:34 up  4:34,  1 user,  load average: 0.24, 0.19, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -227,7 +227,6 @@ void execute(uint64_t n){
       inst[2] = (top->opcode >> 16) & 0xff;
       inst[3] = (top->opcode >> 24) & 0xff;
       if(top->opcode!=0){
-        printf("%x\n",top->opcode);
         disassemble(str, 128, top->pc, inst, 4);
       } else {
         printf("zero opcode\n");
@@ -334,7 +333,6 @@ void execute(uint64_t n){
         inst[3] = (top->opcode >> 24) & 0xff;
         printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
         if(top->opcode!=0){
-          printf("%x\n",top->opcode);
           disassemble(str, 128, top->pc, inst, 4);
           printf("%s\n", str);
         }else{
@@ -362,7 +360,6 @@ void execute(uint64_t n){
       inst[3] = (top->opcode >> 24) & 0xff;
       printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
       if(top->opcode!=0){
-        printf("%x\n",top->opcode);
         disassemble(str, 128, top->pc, inst, 4);
         printf("%s\n", str);
       }else{
@@ -389,7 +386,6 @@ void execute(uint64_t n){
           inst[3] = (top->opcode >> 24) & 0xff;
           printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
           if(top->opcode!=0){
-        printf("%x\n",top->opcode);
             disassemble(str, 128, top->pc, inst, 4);
             printf("%s\n", str);
           }else{
@@ -413,7 +409,6 @@ void execute(uint64_t n){
             inst[3] = (top->opcode >> 24) & 0xff;
             printf("0x%08x: %02x %02x %02x %02x ", top->pc, inst[3], inst[2], inst[1], inst[0]);
             if(top->opcode!=0){
-              printf("%x\n",top->opcode);
               disassemble(str, 128, top->pc, inst, 4);
               printf("%s\n", str);
             }else{
