62577b1d0cec4a9fb0a003c53eeaa7e00faa3384
tracer-ysyx <tracer@ysyx.org>
2026-07-14 21:28:25 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  21:28:25 up 22:33,  1 user,  load average: 0.08, 0.16, 0.09

--- npc/vsrc/CLINT.sv
+++ npc/vsrc/CLINT.sv
@@ -14,7 +14,7 @@ module CLINT(
 );
 
 parameter NUM = 1; //possibly can be changed on start of simulation, may be good
-parameter DEN = 969;
+parameter DEN = 900;
 
 logic [11:0]  divisor; 
 logic [63:0] mtime; 
--- npc/vsrc/alu.sv
+++ npc/vsrc/alu.sv
@@ -60,6 +60,7 @@ module alu (
 
         bus_out_alu_out       = '0;
         bus_out_branch        = '0;
+        csr_imm=0;
 
         if (bus_in_alu_op[5:4] == 2'b00) begin
             case (bus_in_alu_op[2:0])
