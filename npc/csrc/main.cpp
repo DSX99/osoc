d17eed87f0892bdcbdcb928e54fd17b70fd1fdfc
tracer-ysyx <tracer@ysyx.org>
2026-07-15 10:28:32 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  10:28:32 up 1 day,  2:07,  1 user,  load average: 0.20, 0.14, 0.10

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char** argv) {
   #endif
   #ifdef OP_TRACE
   printf("\n\t\t\033[31mRUNNING WITH OP_TRACE\033[0m\n");
-  fp = fopen("/home/dsx99/osoc/ysyx-workbench/npc/tools/idk/opcodes", "wb");
+  fp = fopen("/home/dsx99/osoc/ysyx-workbench/npc/tools/idk/new_microbench", "wb");
   #endif
   Verilated::commandArgs(argc, argv);
   printf("\n\033[1m\033[36mNPC\033[0m\n\n");
