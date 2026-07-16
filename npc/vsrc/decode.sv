b73c33cd0f6e8f6d6f8ec7e79ac21cc26822e1e0
tracer-ysyx <tracer@ysyx.org>
2026-07-16 22:54:00 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  22:54:00 up  1:13,  1 user,  load average: 0.04, 0.05, 0.00

--- npc/vsrc/decode.sv
+++ npc/vsrc/decode.sv
@@ -227,7 +227,6 @@ module decode (
                             bus_out_exception = 1; //raise exception (ecall)
                             bus_out_mcause = 11;
                             bus_out_alu_op        = 8'b10010000;
-                            bus_out_mux_select_pc = 1'b1;
                         end else if(func7 == 7'b0011000 && rs2_val == 5'b00010) begin //mret
                             bus_out_alu_op        = 8'b10010000;
                             bus_out_mux_select_pc = 1'b1;
