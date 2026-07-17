0a5465a095913d5c571ab6efc9e028c2054f16ef
tracer-ysyx <tracer@ysyx.org>
2026-07-17 14:20:04 +0500

    > compile NEMU ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  14:20:04 up  3:48,  1 user,  load average: 0.93, 0.77, 0.50

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int i;
-  int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i++ ) fb[i] = i;
-  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -33,7 +27,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   for(int i=0; i<ctl->w; i++){
     for(int j=0; j<ctl->h; j++){
       if(x+i<w && y+j<h)  {
-        fb[(y+j)*w + x + i] = pixels[j*ctl->w+i];
+        fb[(y+j)*w + x + i] = ~pixels[j*ctl->w+i];
       }
     } 
   }
