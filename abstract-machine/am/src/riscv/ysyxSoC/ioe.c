15861cf5d249f2c54a01696c114b1a94cefc8b67
tracer-ysyx <tracer@ysyx.org>
2026-07-18 17:09:39 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  17:09:39 up 11:40,  1 user,  load average: 0.07, 0.04, 0.02

--- abstract-machine/am/src/riscv/ysyxSoC/ioe.c
+++ abstract-machine/am/src/riscv/ysyxSoC/ioe.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <amdev.h>
 #include <klib-macros.h>
+#include "klib.h"
 #include "riscv.h"
 
 const int ps2_to_am_lut[256] = {
@@ -99,6 +100,31 @@ void __am_uart_rx(AM_UART_RX_T *uart){
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 static void __am_uart_config(AM_UART_CONFIG_T *cfg) { cfg->present = true;  }
+void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  *cfg = (AM_GPU_CONFIG_T) {
+    .present = true, .has_accel = false,
+    .width = 640, .height = 480,
+    .vmemsz = (640) * (480) * sizeof(uint32_t)
+  };
+}
+
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int w = io_read(AM_GPU_CONFIG).width;
+
+  int x = ctl->x;
+  int y = ctl->y;
+  uint32_t *pixels = (uint32_t *)ctl->pixels;
+
+  uint32_t *fb = (uint32_t *)(uintptr_t)(0x21000000);
+
+  for( int i = 0; i<ctl->h; i++){
+    memcpy(&fb[(y+i)*w + x],&pixels[i*ctl->w], ctl->w*sizeof(uint32_t));
+  }
+}
+
+void __am_gpu_status(AM_GPU_STATUS_T *status) {
+  status->ready = true;
+}
 
 
 typedef void (*handler_t)(void *buf);
@@ -108,6 +134,9 @@ static void *lut[128] = {
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
+  [AM_GPU_CONFIG  ] = __am_gpu_config,
+  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
+  [AM_GPU_STATUS  ] = __am_gpu_status,
   [AM_UART_CONFIG ] = __am_uart_config,
   [AM_UART_RX     ] = __am_uart_rx,
 };
--- ysyxSoC/perip/vga/vga_top_apb.v
+++ ysyxSoC/perip/vga/vga_top_apb.v
@@ -20,4 +20,69 @@ module vga_top_apb(
   output        vga_valid
 );
 
+assign in_pready = in_psel & in_penable;
+assign in_prdata = 32'b0;
+assign in_pslverr = 1'b0;
+
+parameter h_frontporch = 96;
+parameter h_active = 144;
+parameter h_backporch = 784;
+parameter h_total = 800;
+
+parameter v_frontporch = 2;
+parameter v_active = 35;
+parameter v_backporch = 515;
+parameter v_total = 525;
+
+reg [7:0] mem [307200];
+
+reg [9:0] x_cnt;
+reg [9:0] y_cnt;
+wire h_valid;
+wire v_valid;
+
+always @(posedge clock) begin
+    if(reset == 1'b1) begin
+        x_cnt <= 1;
+        y_cnt <= 1;
+    end
+    else begin
+        if(x_cnt == h_total)begin
+            x_cnt <= 1;
+            if(y_cnt == v_total) y_cnt <= 1;
+            else y_cnt <= y_cnt + 1;
+        end
+        else x_cnt <= x_cnt + 1;
+    end
+end
+
+always @(posedge clock) begin
+  if(reset) begin
+    for (int i = 0; i < 307200; i = i + 1) begin
+      mem[i]<=0;
+    end
+  end else begin
+    if(in_psel && in_penable) begin
+      if(in_pwrite) begin
+        mem[in_paddr - 32'h2100_0000  ]<=(in_pwdata    )&8'hff;
+        mem[in_paddr - 32'h2100_0000+1]<=(in_pwdata>>8 )&8'hff;
+        mem[in_paddr - 32'h2100_0000+2]<=(in_pwdata>>16)&8'hff;
+        mem[in_paddr - 32'h2100_0000+3]<=(in_pwdata>>24)&8'hff;
+      end
+    end
+  end
+end
+
+assign vga_hsync = (x_cnt > h_frontporch);
+assign vga_vsync = (y_cnt > v_frontporch);
+
+assign h_valid = (x_cnt > h_active) & (x_cnt <= h_backporch);
+assign v_valid = (y_cnt > v_active) & (y_cnt <= v_backporch);
+assign vga_valid = h_valid & v_valid;
+
+wire [9:0] h_addr = h_valid ? (x_cnt - 10'd145) : 10'd0;
+wire [9:0] v_addr = v_valid ? (y_cnt - 10'd36) : 10'd0;
+
+assign {vga_r, vga_g, vga_b} = vga_valid ? {mem[((h_addr + v_addr*640)<<2) + 2], mem[((h_addr + v_addr*640)<<2) + 1], mem[((h_addr + v_addr*640)<<2) + 0]} : 0;
+
 endmodule
