9392caa3108dc8343cbe2f3a745e2b069c69b09d
tracer-ysyx <tracer@ysyx.org>
2026-07-20 23:01:53 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  23:01:53 up  1:08,  1 user,  load average: 0.21, 0.75, 0.97

--- abstract-machine/am/src/riscv/ysyxSoC/ioe.c
+++ abstract-machine/am/src/riscv/ysyxSoC/ioe.c
@@ -80,7 +80,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
       kbd->keycode = ps2_ext_to_am_lut[inl(0x10011000)];
     }else{
       kbd->keydown = 1;
-      kbd->keycode = ps2_ext_to_am_lut[inl(0x10011000)];
+      kbd->keycode = ps2_ext_to_am_lut[data];
     }
     break;  
   default:
@@ -88,7 +88,6 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
     kbd->keycode = ps2_to_am_lut[data];
     break;
   }
-  printf("keycode:%d", kbd->keycode);
 }
 void __am_uart_rx(AM_UART_RX_T *uart){
   if(*(uint8_t *)(0x10000005L) & 1){ //check fifo buffer (0-empty)
