0afbccc00123464a13051d4293b96aaf3c32c2a3
tracer-ysyx <tracer@ysyx.org>
2026-07-20 18:55:01 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  18:55:01 up  2:28,  1 user,  load average: 0.25, 0.12, 0.10

--- npc/vsrc/alu.sv
+++ npc/vsrc/alu.sv
@@ -119,11 +119,7 @@ module alu (
         bus_out_mux_select_pc = bus_in_mux_select_pc;
     
 
-        if (branch) begin
-            bus_out_diff_pc = bus_out_alu_out; 
-        end else begin
-            bus_out_diff_pc = bus_in_pc + 4;
-        end
+        bus_out_diff_pc = bus_in_pc + 4;
 
     end
 
