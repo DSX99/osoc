8168b9495f422bcb7d1e73dc6b5a013bd4ceebdb
tracer-ysyx <tracer@ysyx.org>
2026-07-20 10:54:39 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  10:54:39 up  1:14,  1 user,  load average: 0.17, 0.11, 0.12

--- npc/vsrc/osoc_26000003_core.sv
+++ npc/vsrc/osoc_26000003_core.sv
@@ -260,7 +260,7 @@ module osoc_26000003_core (
     logic flush /*verilator public*/, flush_ex /*verilator public*/;
 
     assign flush = ls_wb_bus_exception_wb;
-    assign flush_ex = (!ex_ls_bus_branch_ex && ex_ls_bus_speculate_ex) && ex_ls_valid_ex && ex_ls_ready_ex && !ex_ls_bus_exception_ls && !ls_wb_bus_exception_wb;  
+    assign flush_ex = (ex_ls_bus_branch_ex != ex_ls_bus_speculate_ex) && ex_ls_valid_ex && ex_ls_ready_ex && !ex_ls_bus_exception_ls && !ls_wb_bus_exception_wb;  
     
     logic [31:0] pc_ifu, next_pc;
 
@@ -491,7 +491,7 @@ module osoc_26000003_core (
         .ex_ls_bus_mux_select_ex    (ex_ls_bus_mux_select_ex),
         .ex_ls_bus_mux_select_pc_ex (ex_ls_bus_mux_select_pc_ex),
         .ex_ls_bus_branch_ex        (ex_ls_bus_branch_ex),
-        .ex_ls_valid_ex             (ex_ls_valid_ex & !flush_ex),
+        .ex_ls_valid_ex             (ex_ls_valid_ex & !(flush_ex && speculation)),
         .ex_ls_ready_ex             (ex_ls_ready_ex),
 
         .ex_ls_bus_mcause_ex        (ex_ls_bus_mcause_ex),
