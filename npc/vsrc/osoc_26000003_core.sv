e33af1a77670de70ceb8c5d47cc48cb6919624d8
tracer-ysyx <tracer@ysyx.org>
2026-07-20 16:55:43 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  16:55:43 up 29 min,  1 user,  load average: 0.13, 0.17, 0.14

--- npc/vsrc/osoc_26000003_core.sv
+++ npc/vsrc/osoc_26000003_core.sv
@@ -280,7 +280,7 @@ module osoc_26000003_core (
         .valid(if_de_valid_if), .ready(if_de_ready_if),
         .cache_addr(cache_addr), .cache_valid(cache_valid), .cache_opcode(cache_opcode), .cache_ready(cache_ready),
         .do_spec(do_spec), .addr_spec(addr_spec),
-        .pc_to_write(de_ex_bus_pc_ex), .offset_to_write(de_ex_bus_imm_ex[12:1]), .write(ex_ls_bus_branch_ex && !ex_ls_bus_speculate_ex)
+        .pc_to_write(de_ex_bus_pc_ex), .offset_to_write(de_ex_bus_imm_ex[12:1]), .write(ex_ls_bus_branch_ex && !ex_ls_bus_speculate_ex && de_ex_bus_alu_op[6])
     );
 
     icache icache_mod(
