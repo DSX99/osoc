67214df2a51cfb62c697db04b41673cefe9a09cb
tracer-ysyx <tracer@ysyx.org>
2026-07-21 22:25:40 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  22:25:40 up 14:30,  1 user,  load average: 0.20, 0.24, 0.16

--- npc/csrc/main_npc.cpp
+++ npc/csrc/main_npc.cpp
@@ -409,29 +409,9 @@ void execute(uint64_t n){
           difftest_regcpy(&cpu, 1);
           device_access--;
           printf("device call -- value:%d\n", device_access);
-          for(int i = 0; i < 16; i++){
-            printf("regs %d:%x, npc:%x\n",i, cpu.gpr[i], top->reg_mod->regs[i]);
-          }for(int i = 0; i < 16; i++){
-            printf("regs %d:%x\n",i+16, cpu.gpr[i+16]);
-          }
-        }
-
-        printf("1\n");
-        for(int i = 0; i < 16; i++){
-          printf("regs %d:%x, npc:%x\n",i, cpu.gpr[i], top->reg_mod->regs[i]);
-        }for(int i = 0; i < 16; i++){
-          printf("regs %d:%x\n",i+16, cpu.gpr[i+16]);
         }
 
         difftest_regcpy(&ref_cpu, 0);
-
-        
-        printf("2\n");
-        for(int i = 0; i < 16; i++){
-          printf("regs %d:%x, npc:%x\n",i, cpu.gpr[i], top->reg_mod->regs[i]);
-        }for(int i = 0; i < 16; i++){
-          printf("regs %d:%x\n",i+16, cpu.gpr[i+16]);
-        }
 
         if (ref_cpu.pc != top->pc) {
           printf("Difference with REF pc, should:0x%08x, actually:0x%08x\n", ref_cpu.pc, top->pc);
