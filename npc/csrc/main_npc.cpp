4775b9ad418e101975d51f945d24916b602009a8
tracer-ysyx <tracer@ysyx.org>
2026-07-21 22:37:44 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  22:37:44 up 14:42,  1 user,  load average: 0.15, 0.14, 0.15

--- npc/csrc/main_npc.cpp
+++ npc/csrc/main_npc.cpp
@@ -295,7 +295,7 @@ void execute(uint64_t n){
       program[stage].possible_branch_count++;
     }
 
-    if (top->reg_valid) {
+    if (top->reg_valid_e) {
       program[stage].writeback++;
       if (top->branch_taken) program[stage].branch_taken++;
       if (top->flush)        program[stage].flush++;
