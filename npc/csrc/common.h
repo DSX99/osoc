9a71d62422545ff5b748f4a31b4f28675b7476a1
tracer-ysyx <tracer@ysyx.org>
2026-07-22 12:48:32 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  12:48:32 up 34 min,  1 user,  load average: 0.65, 0.49, 0.58

--- npc/csrc/common.h
+++ npc/csrc/common.h
@@ -37,7 +37,7 @@ void print_stage_performance_table(const uint64_t cycles[3], const Performance_t
 
 
 
-// #define CONFIG_FST
+#define CONFIG_FST
 // #define MTRACE
 // #define ITRACE
 // #define OP_TRACE
