563cb6227d74a3509293a001c1edb4084e025486
tracer-ysyx <tracer@ysyx.org>
2026-07-22 11:27:40 +0500

    > sim RTL ysyx_22040000 Nikita Linux dsx-99 6.18.33.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun 18 21:54:43 UTC 2026 x86_64 x86_64 x86_64 GNU/Linux  11:27:40 up 12 min,  1 user,  load average: 0.35, 0.63, 0.50

--- npc/csrc/common.h
+++ npc/csrc/common.h
@@ -1,22 +1,23 @@
-// #include "VysyxSoCFull.h"
-// #include "VysyxSoCFull___024root.h"
-// #include "VysyxSoCFull_osoc_26000003.h"
-// #include "VysyxSoCFull_osoc_26000003_core.h"
-// #include "VysyxSoCFull_ysyxSoCFull.h"
-// #include "VysyxSoCFull_ysyxSoCASIC.h"
-// #include "VysyxSoCFull_CPU.h"
-// #include "VysyxSoCFull_regs.h"
-// #define SOC
+#ifdef SOC
+    #include "VysyxSoCFull.h"
+    #include "VysyxSoCFull___024root.h"
+    #include "VysyxSoCFull_osoc_26000003.h"
+    #include "VysyxSoCFull_osoc_26000003_core.h"
+    #include "VysyxSoCFull_ysyxSoCFull.h"
+    #include "VysyxSoCFull_ysyxSoCASIC.h"
+    #include "VysyxSoCFull_CPU.h"
+    #include "VysyxSoCFull_regs.h"
+#endif
 
 
 
-#include "Vosoc_26000003_func.h"
-#include "Vosoc_26000003_func___024root.h"
-#include "Vosoc_26000003_func_osoc_26000003_func.h"
-#include "Vosoc_26000003_func_osoc_26000003_core.h"
-#include "Vosoc_26000003_func_regs.h"
-#define NPC
+    #include "Vosoc_26000003_func.h"
+    #include "Vosoc_26000003_func___024root.h"
+    #include "Vosoc_26000003_func_osoc_26000003_func.h"
+    #include "Vosoc_26000003_func_osoc_26000003_core.h"
+    #include "Vosoc_26000003_func_regs.h"
 
+#
 
 
 typedef struct Performance_t {
@@ -37,9 +38,9 @@ void print_stage_performance_table(const uint64_t cycles[3], const Performance_t
 
 
 
-// #define CONFIG_FST
+#define CONFIG_FST
 // #define MTRACE
-// #define ITRACE
+#define ITRACE
 // #define OP_TRACE
 
 #define MAX_SIM_TIME 1000LL*1024LL*1024LL*1024LL
